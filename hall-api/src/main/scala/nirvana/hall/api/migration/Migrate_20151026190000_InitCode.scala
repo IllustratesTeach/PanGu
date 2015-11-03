4c65d300b0bb74faf666912fe94091d3afa178a0
songpeng <songpeng729@126.com>
2015-11-03 10:57:09 +0800

    初始化字典数据包括行政区划

---
+++ hall-api/src/main/scala/nirvana/hall/api/migration/Migrate_20151026190000_InitCode.scala
@@ -0,0 +1,20 @@
+package nirvana.hall.api.migration
+
+import monad.migration.Migration
+
+import scala.io.Source
+
+/**
+ * Created by songpeng on 15/10/26.
+ * 初始化字典数据，行政区划
+ */
+class Migrate_20151026190000_InitCode extends Migration{
+  override def up(): Unit = {
+    execute(Source.fromInputStream(getClass.getResourceAsStream("Migration_20151026190000_Code.sql")).mkString)
+    execute(Source.fromInputStream(getClass.getResourceAsStream("Migration_20151026190000_Area.sql")).mkString)
+  }
+
+  override def down(): Unit = {
+
+  }
+}
