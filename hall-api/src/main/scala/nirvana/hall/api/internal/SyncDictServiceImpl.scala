5ffe43ef738de361adeba0abbd3822c38eff9a21
songpeng <songpeng729@126.com>
2015-11-06 17:49:29 +0800

    字典同步

--- hall-api/src/main/scala/nirvana/hall/api/internal/SyncDictServiceImpl.scala
+++ hall-api/src/main/scala/nirvana/hall/api/internal/SyncDictServiceImpl.scala
@@ -3,14 +3,48 @@ package nirvana.hall.api.internal
 import nirvana.hall.api.entities._
 import nirvana.hall.api.services.SyncDictService
 import nirvana.hall.protocol.sys.SyncDictProto.SyncDictResponse._
-import nirvana.hall.protocol.sys.SyncDictProto.{SyncDictResponse, DictType}
+import nirvana.hall.protocol.sys.SyncDictProto.{SyncDictRequest, DictType, SyncDictResponse}
+import org.apache.tapestry5.ioc.annotations.PostInjection
+import org.apache.tapestry5.ioc.services.cron.{CronSchedule, PeriodicExecutor}
+import org.springframework.transaction.annotation.Transactional
+import scalikejdbc._
+import scala.collection.JavaConversions._
 
 /**
  * Created by songpeng on 15/11/4.
  * 字典同步服务
  */
 class SyncDictServiceImpl extends SyncDictService{
+  private val REMOTE_SERVICE_URL = "http://127.0.0.1:9081";
 
+  /**
+   * 访问远程服务器，同步字典
+   */
+  @PostInjection
+  def startUp(periodicExecutor: PeriodicExecutor): Unit = {
+    periodicExecutor.addJob(new CronSchedule("0 0 0 * * ? *"), "sync-remote-dict", new Runnable {
+      override def run(): Unit = {
+        val request = SyncDictRequest.newBuilder()
+        val response = SyncDictResponse.newBuilder()
+        DictType.values().foreach{ f =>
+          request.setDictType(f)
+          WebAppClientUtils.call(REMOTE_SERVICE_URL, request.build(), response)
+          syncDict(f, response.getSyncDataList.toSeq)
+        }
+      }
+    })
+  }
+
+  /**
+   * 将scala对象转换为SyncDict类型
+   * @param o
+   * @return
+   */
+  def convert2CodeData(o: Any): SyncData ={
+    val data = CodeData.newBuilder()
+    ScalaUtils.convertScalaToProtobuf(o, data)
+    SyncData.newBuilder().setExtension(CodeData.data, data.build()).build()
+  }
   /**
    * 根据字典类型获取全部字典数据
    * @param dictType 字典类型
@@ -21,252 +55,170 @@ class SyncDictServiceImpl extends SyncDictService{
       case DictType.CODE_AJLB =>
         CodeAjlb.findAll().map { f =>
           val data = CodeData.newBuilder()
-          data.setCode(f.code)
-          f.name.foreach(data.setName)
-          f.deleteFlag.foreach(data.setDeleteFlag)
-          f.remark.foreach(data.setRemark)
-          f.ord.foreach(data.setOrd)
+          ScalaUtils.convertScalaToProtobuf(f, data)
           f.firstLetter.foreach(data.setOther)
           SyncData.newBuilder().setExtension(CodeData.data, data.build()).build()
         }.toSeq
       case DictType.CODE_CJLB =>
-        CodeCjlb.findAll().map{ f =>
-          val data = CodeData.newBuilder()
-          data.setCode(f.code)
-          f.name.foreach(data.setName)
-          f.deleteFlag.foreach(data.setDeleteFlag)
-          f.remark.foreach(data.setRemark)
-          f.ord.foreach(data.setOrd)
-          SyncData.newBuilder().setExtension(CodeData.data, data.build()).build()
-        }.toSeq
+        CodeCjlb.findAll().map(convert2CodeData).toSeq
       case DictType.CODE_GJ =>
-        CodeGj.findAll().map{ f =>
-          val data = CodeData.newBuilder()
-          data.setCode(f.code)
-          f.name.foreach(data.setName)
-          f.deleteFlag.foreach(data.setDeleteFlag)
-          f.remark.foreach(data.setRemark)
-          f.ord.foreach(data.setOrd)
-          SyncData.newBuilder().setExtension(CodeData.data, data.build()).build()
-        }.toSeq
+        CodeGj.findAll().map(convert2CodeData).toSeq
       case DictType.CODE_JJLY =>
-        CodeJjly.findAll().map{ f =>
-          val data = CodeData.newBuilder()
-          data.setCode(f.code)
-          f.name.foreach(data.setName)
-          f.deleteFlag.foreach(data.setDeleteFlag)
-          f.remark.foreach(data.setRemark)
-          f.ord.foreach(data.setOrd)
-          SyncData.newBuilder().setExtension(CodeData.data, data.build()).build()
-        }.toSeq
+        CodeJjly.findAll().map(convert2CodeData).toSeq
       case DictType.CODE_LY =>
-        CodeLy.findAll().map{ f =>
-          val data = CodeData.newBuilder()
-          data.setCode(f.code)
-          f.name.foreach(data.setName)
-          f.deleteFlag.foreach(data.setDeleteFlag)
-          f.remark.foreach(data.setRemark)
-          f.ord.foreach(data.setOrd)
-          SyncData.newBuilder().setExtension(CodeData.data, data.build()).build()
-        }.toSeq
+        CodeLy.findAll().map(convert2CodeData).toSeq
       case DictType.CODE_MARRY =>
-        CodeMarry.findAll().map{ f =>
-          val data = CodeData.newBuilder()
-          data.setCode(f.code)
-          f.name.foreach(data.setName)
-          f.deleteFlag.foreach(data.setDeleteFlag)
-          f.remark.foreach(data.setRemark)
-          f.ord.foreach(data.setOrd)
-          SyncData.newBuilder().setExtension(CodeData.data, data.build()).build()
-        }.toSeq
+        CodeMarry.findAll().map(convert2CodeData).toSeq
       case DictType.CODE_MZ =>
-        CodeMz.findAll().map{ f =>
-          val data = CodeData.newBuilder()
-          data.setCode(f.code)
-          f.name.foreach(data.setName)
-          f.deleteFlag.foreach(data.setDeleteFlag)
-          f.remark.foreach(data.setRemark)
-          f.ord.foreach(data.setOrd)
-          SyncData.newBuilder().setExtension(CodeData.data, data.build()).build()
-        }.toSeq
+        CodeMz.findAll().map(convert2CodeData).toSeq
       case DictType.CODE_RYLX =>
         CodeRylx.findAll().map{ f =>
           val data = CodeData.newBuilder()
-          data.setCode(f.code)
-          f.name.foreach(data.setName)
-          f.deleteFlag.foreach(data.setDeleteFlag)
-          f.remark.foreach(data.setRemark)
-          f.ord.foreach(data.setOrd)
+          ScalaUtils.convertScalaToProtobuf(f, data)
           f.strtype.foreach(data.setOther)
-          data.build()
           SyncData.newBuilder().setExtension(CodeData.data, data.build()).build()
         }.toSeq
       case DictType.CODE_TSRQ =>
-        CodeTsrq.findAll().map{ f =>
-          val data = CodeData.newBuilder()
-          data.setCode(f.code)
-          f.name.foreach(data.setName)
-          f.deleteFlag.foreach(data.setDeleteFlag)
-          f.remark.foreach(data.setRemark)
-          f.ord.foreach(data.setOrd)
-          SyncData.newBuilder().setExtension(CodeData.data, data.build()).build()
-        }.toSeq
+        CodeTsrq.findAll().map(convert2CodeData).toSeq
       case DictType.CODE_TSSF =>
-        CodeTssf.findAll().map{ f =>
-          val data = CodeData.newBuilder()
-          data.setCode(f.code)
-          f.name.foreach(data.setName)
-          f.deleteFlag.foreach(data.setDeleteFlag)
-          f.remark.foreach(data.setRemark)
-          f.ord.foreach(data.setOrd)
-          SyncData.newBuilder().setExtension(CodeData.data, data.build()).build()
-        }.toSeq
+        CodeTssf.findAll().map(convert2CodeData).toSeq
       case DictType.CODE_WHCD =>
-        CodeWhcd.findAll().map{ f =>
-          val data = CodeData.newBuilder()
-          data.setCode(f.code)
-          f.name.foreach(data.setName)
-          f.deleteFlag.foreach(data.setDeleteFlag)
-          f.remark.foreach(data.setRemark)
-          f.ord.foreach(data.setOrd)
-          SyncData.newBuilder().setExtension(CodeData.data, data.build()).build()
-        }.toSeq
+        CodeWhcd.findAll().map(convert2CodeData).toSeq
       case DictType.CODE_XB =>
-        CodeXb.findAll().map{ f =>
-          val data = CodeData.newBuilder()
-          data.setCode(f.code)
-          f.name.foreach(data.setName)
-          f.deleteFlag.foreach(data.setDeleteFlag)
-          f.remark.foreach(data.setRemark)
-          f.ord.foreach(data.setOrd)
-          SyncData.newBuilder().setExtension(CodeData.data, data.build()).build()
-        }.toSeq
+        CodeXb.findAll().map(convert2CodeData).toSeq
       case DictType.CODE_ZC =>
-        CodeZc.findAll().map{ f =>
-          val data = CodeData.newBuilder()
-          data.setCode(f.code)
-          f.name.foreach(data.setName)
-          f.deleteFlag.foreach(data.setDeleteFlag)
-          f.remark.foreach(data.setRemark)
-          f.ord.foreach(data.setOrd)
-          SyncData.newBuilder().setExtension(CodeData.data, data.build()).build()
-        }.toSeq
+        CodeZc.findAll().map(convert2CodeData).toSeq
       case DictType.CODE_ZJXY =>
-        CodeZjxy.findAll().map{ f =>
-          val data = CodeData.newBuilder()
-          data.setCode(f.code)
-          f.name.foreach(data.setName)
-          f.deleteFlag.foreach(data.setDeleteFlag)
-          f.remark.foreach(data.setRemark)
-          f.ord.foreach(data.setOrd)
-          SyncData.newBuilder().setExtension(CodeData.data, data.build()).build()
-        }.toSeq
+        CodeZjxy.findAll().map(convert2CodeData).toSeq
       case DictType.CODE_ZZMM =>
-        CodeZzmm.findAll().map{ f =>
-          val data = CodeData.newBuilder()
-          data.setCode(f.code)
-          f.name.foreach(data.setName)
-          f.deleteFlag.foreach(data.setDeleteFlag)
-          f.remark.foreach(data.setRemark)
-          f.ord.foreach(data.setOrd)
-          SyncData.newBuilder().setExtension(CodeData.data, data.build()).build()
-        }.toSeq
+        CodeZzmm.findAll().map(convert2CodeData).toSeq
       case DictType.GAFIS_GATHER_NODE =>
-        GafisGatherNode.findAll().map(gafisGafisNode2SyncData).toSeq
+        GafisGatherNode.findAll().map{ f =>
+          val data = GatherNode.newBuilder()
+          ScalaUtils.convertScalaToProtobuf(f, data)
+          SyncData.newBuilder().setExtension(GatherNode.data, data.build()).build()
+        }.toSeq
       case DictType.GAFIS_GATHER_TYPE =>
-        GafisGatherType.findAll().map(gafisGatherType2SyncData).toSeq
+        GafisGatherType.findAll().map{ f =>
+          val data = GatherType.newBuilder()
+          ScalaUtils.convertScalaToProtobuf(f, data)
+          SyncData.newBuilder().setExtension(GatherType.data, data.build()).build()
+        }.toSeq
       case DictType.GAFIS_GATHER_TYPE_NODE_FIELD =>
-        GafisGatherTypeNodeField.findAll().map(gafisGatherTypeNodeField2SyncData).toSeq
+        GafisGatherTypeNodeField.findAll().map{ f =>
+          val data = GatherTypeNodeField.newBuilder()
+          ScalaUtils.convertScalaToProtobuf(f, data)
+          SyncData.newBuilder().setExtension(GatherTypeNodeField.data, data.build()).build()
+        }.toSeq
       case DictType.SYS_DEPART =>
-        SysDepart.findAll().map(sysDepart2SyncData).toSeq
+        SysDepart.findAll().map{ f =>
+          val data = Depart.newBuilder()
+          ScalaUtils.convertScalaToProtobuf(f, data)
+          SyncData.newBuilder().setExtension(Depart.data, data.build()).build()
+        }.toSeq
       case _ =>
         Nil
     }
   }
 
   /**
-   * 采集节点，数据类型转换为proto格式
-   * @param gafisGatherNode 采集节点
+   * 同步字典
+   * @param dictType 字典类型
+   * @param syncDataList 要同步的字典数据
+   * @param session
    * @return
    */
-  def gafisGafisNode2SyncData(gafisGatherNode: GafisGatherNode): SyncData = {
-    val data = GatherNode.newBuilder()
-    data.setPkId(gafisGatherNode.pkId)
-    data.setNodeRequest(gafisGatherNode.nodeRequest)
-    data.setCreateUserId(gafisGatherNode.createUserId)
-    data.setCreateDatetime(gafisGatherNode.createDatetime.getMillis)
-    gafisGatherNode.nodeCode.foreach(data.setNodeCode)
-    gafisGatherNode.nodeName.foreach(data.setNodeName)
-    gafisGatherNode.nodeImg.foreach(data.setNodeImg)
-    gafisGatherNode.updateUserId.foreach(data.setUpdateUserId)
-    gafisGatherNode.updateDatetime.foreach(t => data.setUpdateDatetime(t.getMillis))
-    gafisGatherNode.deleteFlag.foreach(t => data.setDeleteFlag(t.toString))
-    SyncData.newBuilder().setExtension(GatherNode.data, data.build()).build()
+  @Transactional
+  override def syncDict(dictType: DictType, syncDataList: Seq[SyncData])(implicit session: DBSession): Unit = {
+    //删除原有的数据
+    deleteDictData(dictType)
+    syncDataList.foreach{ f =>
+      if(f.hasExtension(CodeData.data)){
+        val data = f.getExtension(CodeData.data)
+        dictType match {
+          case DictType.CODE_AJLB =>
+            val code = ScalaUtils.convertProtobufToScala[CodeAjlb](data)
+            CodeAjlb.create(code.code,code.name,code.deleteFlag,code.ord,code.remark,Option(data.getOther()))
+          case DictType.CODE_CJLB =>
+            val code = ScalaUtils.convertProtobufToScala[CodeCjlb](data)
+            CodeCjlb.create(code.code,code.name,code.deleteFlag,code.ord,code.remark)
+          case DictType.CODE_GJ =>
+            val code = ScalaUtils.convertProtobufToScala[CodeGj](data)
+            CodeGj.create(code.code,code.name,code.deleteFlag,code.ord,code.remark)
+          case DictType.CODE_JJLY =>
+            val code = ScalaUtils.convertProtobufToScala[CodeJjly](data)
+            CodeJjly.create(code.code,code.name,code.deleteFlag,code.ord,code.remark)
+          case DictType.CODE_LY =>
+            val code = ScalaUtils.convertProtobufToScala[CodeLy](data)
+            CodeLy.create(code.code,code.name,code.deleteFlag,code.ord,code.remark)
+          case DictType.CODE_MARRY =>
+            val code = ScalaUtils.convertProtobufToScala[CodeMarry](data)
+            CodeMarry.create(code.code,code.name,code.deleteFlag,code.ord,code.remark)
+          case DictType.CODE_MZ =>
+            val code = ScalaUtils.convertProtobufToScala[CodeMz](data)
+            CodeMz.create(code.code,code.name,code.deleteFlag,code.ord,code.remark)
+          case DictType.CODE_RYLX =>
+            val code = ScalaUtils.convertProtobufToScala[CodeRylx](data)
+            CodeRylx.create(code.code,code.name,code.deleteFlag,code.ord,code.remark)
+          case DictType.CODE_TSRQ =>
+            val code = ScalaUtils.convertProtobufToScala[CodeTsrq](data)
+            CodeTsrq.create(code.code,code.name,code.deleteFlag,code.ord,code.remark)
+          case DictType.CODE_TSSF =>
+            val code = ScalaUtils.convertProtobufToScala[CodeTssf](data)
+            CodeTssf.create(code.code,code.name,code.deleteFlag,code.ord,code.remark)
+          case DictType.CODE_WHCD =>
+            val code = ScalaUtils.convertProtobufToScala[CodeWhcd](data)
+            CodeWhcd.create(code.code,code.name,code.deleteFlag,code.ord,code.remark)
+          case DictType.CODE_XB =>
+            val code = ScalaUtils.convertProtobufToScala[CodeXb](data)
+            CodeXb.create(code.code,code.name,code.deleteFlag,code.ord,code.remark)
+          case DictType.CODE_ZC =>
+            val code = ScalaUtils.convertProtobufToScala[CodeZc](data)
+            CodeZc.create(code.code,code.name,code.deleteFlag,code.ord,code.remark)
+          case DictType.CODE_ZJXY =>
+            val code = ScalaUtils.convertProtobufToScala[CodeZjxy](data)
+            CodeZjxy.create(code.code,code.name,code.deleteFlag,code.ord,code.remark)
+          case DictType.CODE_ZZMM =>
+            val code = ScalaUtils.convertProtobufToScala[CodeZzmm](data)
+            CodeZzmm.create(code.code,code.name,code.deleteFlag,code.ord,code.remark)
+          case _ =>
+            Nil
+        }
+      }
+      else if(f.hasExtension(GatherType.data)){
+        val data = f.getExtension(GatherType.data)
+        val o = ScalaUtils.convertProtobufToScala[GafisGatherType](data)
+        GafisGatherType.create(o.pkId,o.typeName,o.deleteFlag,o.createUserId,o.createDatetime,o.updateUserId,o.updateDatetime,o.menuId,o.personCategory,o.gatherCategory,o.parentId,o.ischildren,o.ruleId)
+      }
+      else if(f.hasExtension(GatherNode.data)){
+        val data = f.getExtension(GatherNode.data)
+        val o = ScalaUtils.convertProtobufToScala[GafisGatherNode](data)
+        GafisGatherNode.create(o.pkId,o.nodeCode,o.nodeName,o.nodeRequest,o.deleteFlag,o.createUserId,o.createDatetime,o.updateUserId,o.updateDatetime,o.nodeImg)
+      }
+      else if(f.hasExtension(GatherTypeNodeField.data)){
+        val data = f.getExtension(GatherTypeNodeField.data)
+        val o = ScalaUtils.convertProtobufToScala[GafisGatherTypeNodeField](data)
+        GafisGatherTypeNodeField.create(o.pkId,o.typeId,o.nodeId,o.fieldId,o.required,o.departId)
+      }
+      else if(f.hasExtension(Depart.data)){
+        val data = f.getExtension(Depart.data)
+        val o = ScalaUtils.convertProtobufToScala[SysDepart](data)
+        SysDepart.create(o.code,o.name,o.leader,o.remark,o.deleteFlag,o.isLeaf,o.parentId,o.deptLevel,o.longitude,o.latitude,o.phone,o.longName,o.isHaveChamber,o.chamberType,o.isSpecial,o.integrationType)
+      }
+    }
+
   }
 
   /**
-   * 采集类型，数据类型转换为proto格式
-   * @param gafisGatherType 采集类型
+   * 根据字典类型删除字典数据
+   * @param dictType 字典类型
+   * @param session
    * @return
    */
-  def gafisGatherType2SyncData(gafisGatherType: GafisGatherType): SyncData = {
-    val data = GatherType.newBuilder()
-    data.setPkId(gafisGatherType.pkId)
-    data.setTypeName(gafisGatherType.typeName)
-    data.setCreateUserId(gafisGatherType.createUserId)
-    data.setCreateDatetime(gafisGatherType.createDatetime.getMillis)
-    gafisGatherType.updateUserId.foreach(data.setUpdateUserId)
-    gafisGatherType.updateDatetime.foreach(t => data.setUpdateDatetime(t.getMillis))
-    gafisGatherType.menuId.foreach(data.setMenuId)
-    gafisGatherType.personCategory.foreach(data.setPersonCategory)
-    gafisGatherType.gatherCategory.foreach(data.setGatherCategory)
-    gafisGatherType.parentId.foreach(data.setParentId)
-    gafisGatherType.ischildren.foreach(data.setIsChildren)
-    gafisGatherType.ruleId.foreach(data.setRuleId)
-    gafisGatherType.deleteFlag.foreach(t => data.setDeleteFlag(t.toString))
-    SyncData.newBuilder().setExtension(GatherType.data, data.build()).build()
-  }
-
-  /**
-   * 采集字段，转换为proto格式
-   * @param gafisGatherTypeNodeField 采集字段
-   * @return
-   */
-  def gafisGatherTypeNodeField2SyncData(gafisGatherTypeNodeField: GafisGatherTypeNodeField): SyncData = {
-    val data = GatherTypeNodeField.newBuilder()
-    data.setPkId(gafisGatherTypeNodeField.pkId)
-    gafisGatherTypeNodeField.typeId.foreach(data.setTypeId)
-    gafisGatherTypeNodeField.nodeId.foreach(data.setNodeId)
-    gafisGatherTypeNodeField.fieldId.foreach(data.setFieldId)
-    gafisGatherTypeNodeField.required.foreach(data.setRequired(_))
-    gafisGatherTypeNodeField.departId.foreach(data.setDepartId)
-    SyncData.newBuilder().setExtension(GatherTypeNodeField.data, data.build()).build()
-  }
-
-  /**
-   * 单位，转换为proto格式
-   * @param sysDepart 单位
-   * @return
-   */
-  def sysDepart2SyncData(sysDepart: SysDepart): SyncData = {
-    val data = Depart.newBuilder()
-    data.setCode(sysDepart.code)
-    sysDepart.name.foreach(data.setName)
-    sysDepart.leader.foreach(data.setLeader)
-    sysDepart.remark.foreach(data.setRemark)
-    sysDepart.isLeaf.foreach(data.setIsLeaf)
-    sysDepart.parentId.foreach(data.setParentId)
-    sysDepart.deptLevel.foreach(data.setDeptLevel)
-    sysDepart.longitude.foreach(data.setLongitude)
-    sysDepart.latitude.foreach(data.setLatitude)
-    sysDepart.phone.foreach(data.setPhone)
-    sysDepart.longName.foreach(data.setLongName)
-    sysDepart.isHaveChamber.foreach(data.setIsHaveChamber)
-    sysDepart.chamberType.foreach(data.setChamberType(_))
-    sysDepart.isSpecial.foreach(data.setIsSpecial(_))
-    sysDepart.integrationType.foreach(data.setIntegrationType)
-    sysDepart.deleteFlag.foreach(data.setDeleteFlag)
-    SyncData.newBuilder().setExtension(Depart.data, data.build()).build()
+  def deleteDictData(dictType: DictType)(implicit session: DBSession): Unit ={
+    val tableName = dictType.toString
+    if(!tableName.isEmpty)
+      SQL("delete from "+ tableName).update().apply()
   }
 
 }
--- hall-api/src/main/scala/nirvana/hall/api/services/SyncDictService.scala
+++ hall-api/src/main/scala/nirvana/hall/api/services/SyncDictService.scala
@@ -2,6 +2,8 @@ package nirvana.hall.api.services
 
 import nirvana.hall.protocol.sys.SyncDictProto.DictType
 import nirvana.hall.protocol.sys.SyncDictProto.SyncDictResponse.SyncData
+import org.springframework.transaction.annotation.Transactional
+import scalikejdbc.DBSession
 
 /**
  * 字典同步 service
@@ -15,5 +17,15 @@ trait SyncDictService {
    * @return
    */
   def findAllDict(dictType: DictType): Seq[SyncData]
+
+  /**
+   * 同步字典
+   * @param dictType 字典类型
+   * @param syncDataList 要同步的字典数据
+   * @param session
+   * @return
+   */
+  @Transactional
+  def syncDict(dictType: DictType, syncDataList: Seq[SyncData])(implicit session: DBSession = AutoSpringDataSourceSession.apply())
 
 }
