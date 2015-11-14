e7d8b147a737582d48e57b947ef549dde07c693c
Jun Tsai <jcai@ganshane.com>
2015-11-14 16:50:34 +0800

    调整TPCard的过滤器

--- hall-api/src/main/scala/nirvana/hall/v62/internal/filter/AddTPCardFilter.scala
+++ hall-api/src/main/scala/nirvana/hall/v62/internal/filter/AddTPCardFilter.scala
@@ -1,12 +1,11 @@
 package nirvana.hall.v62.internal.filter
 
-import nirvana.hall.api.services.{ProtobufRequestHandler, ProtobufRequestFilter}
-import nirvana.hall.protocol.sys.CommonProto.{BaseResponse, BaseRequest}
-import nirvana.hall.protocol.v62.AddTPCardProto.{AddTPCardResponse, AddTPCardRequest}
+import nirvana.hall.api.services.{ProtobufRequestFilter, ProtobufRequestHandler}
+import nirvana.hall.protocol.sys.CommonProto.{BaseRequest, BaseResponse}
+import nirvana.hall.protocol.v62.AddTPCardProto.{AddTPCardRequest, AddTPCardResponse}
 import nirvana.hall.v62.config.HallV62Config
 import nirvana.hall.v62.internal.V62Facade
 import nirvana.hall.v62.internal.c.gloclib.galoctpConverter
-import nirvana.hall.v62.services.{DatabaseTable, V62ServerAddress}
 
 /**
  * Add TPCard Filter
@@ -17,9 +16,6 @@ class AddTPCardFilter(facade:V62Facade,config:HallV62Config) extends ProtobufReq
   override def handle(protobufRequest: BaseRequest, responseBuilder: BaseResponse.Builder, handler: ProtobufRequestHandler): Boolean = {
     if(protobufRequest.hasExtension(AddTPCardRequest.cmd)){
       val request = protobufRequest.getExtension(AddTPCardRequest.cmd)
-      val address = V62ServerAddress(config.host,
-        config.port,config.user,Option(config.password))
-      val database = DatabaseTable(config.templateTable.dbId,config.templateTable.tableId)
 
       val tpCard = galoctpConverter.convertProtoBuf2GTPCARDINFOSTRUCT(request.getCard)
       facade.NET_GAFIS_FLIB_Add(config.templateTable.dbId.toShort,
