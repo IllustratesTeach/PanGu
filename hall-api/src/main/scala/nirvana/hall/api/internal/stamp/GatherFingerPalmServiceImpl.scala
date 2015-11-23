7248b5b7559e7d81d9bd7c5510635fd57c2e668e
anmi <anmiyy@sina.cn>
2015-11-23 10:52:21 +0800

    service实现

--- hall-api/src/main/scala/nirvana/hall/api/internal/stamp/GatherFingerPalmServiceImpl.scala
+++ hall-api/src/main/scala/nirvana/hall/api/internal/stamp/GatherFingerPalmServiceImpl.scala
@@ -1,13 +1,39 @@
 package nirvana.hall.api.internal.stamp
 
-import nirvana.hall.api.entities.{GafisGatherPalm, GafisGatherFinger, SysUser}
+import java.util
+import javax.sql.rowset.serial.SerialBlob
+
+import nirvana.hall.api.entities.{GafisGatherPalm, GafisGatherFinger}
+import nirvana.hall.api.internal.AnalysisData
 import nirvana.hall.api.services.stamp.GatherFingerPalmService
+import org.joda.time.DateTime
 import scalikejdbc._
 
 /**
  * Created by wangjue on 2015/10/27.
  */
 class GatherFingerPalmServiceImpl extends GatherFingerPalmService{
+
+  /**
+   * 查找人员全部指纹信息
+   * @param personId
+   * @param session
+   * @return
+   */
+  override def queryFingerInfoByPersonId(personId : String)(implicit session: DBSession = GafisGatherFinger.autoSession) : List[GafisGatherFinger] = {
+    GafisGatherFinger.findAllBy(sqls.eq(GafisGatherFinger.column.personId,personId))
+  }
+
+  /**
+   * 查询指纹图像数据
+   * @param personId
+   * @param session
+   * @return
+   */
+  override def queryFingerDataByPersonId(personId: String)(implicit session: DBSession): List[GafisGatherFinger] = {
+    GafisGatherFinger.findAllBy(sqls.eq(GafisGatherFinger.column.personId,personId).and.eq(GafisGatherFinger.column.lobtype,1).orderBy(GafisGatherFinger.column.fgp))
+  }
+
   /**
    * 查询指纹信息
    * @param personId
@@ -29,9 +55,26 @@ class GatherFingerPalmServiceImpl extends GatherFingerPalmService{
   /**
    * 指掌纹添加
    * @param fingerPalmData
-   * @param login
    */
-  override def addFingerPalmData(fingerPalmData: String, login: SysUser) (implicit session: DBSession) : Boolean = ???
+  override def addFingerPalmData(fingerPalmData: String,personId: String) (implicit session: DBSession) : String = {
+    val listData : util.List[util.HashMap[_, _]] = AnalysisData.analysisFinger(fingerPalmData);
+    deleteFingerPalmData(personId);//采集指纹数据前删除旧数据
+    for (v <- 0 to listData.size()-1) {
+      val map : util.HashMap[_, _] = listData.get(v)
+      val pkId = map.get("pkId").toString
+      val fgp = map.get("fgp").toString
+      val fgpCase = map.get("fgpCase").toString
+      val groupId = map.get("groupId").toString
+      val lobType = map.get("lobType").toString
+      val gatherData : SerialBlob = map.get("gatherData").asInstanceOf[SerialBlob]
+      GafisGatherFinger.create(pkId,Some(personId),fgp.toShort,Some(groupId.toShort),lobType.toShort,None,
+            DateTime.now(),None,Some(DateTime.now()),Some(fgpCase),None,None,None,None,gatherData,None,None,None)
+    }
+
+    val info : List[GafisGatherFinger] = queryFingerInfoByPersonId(personId)
+
+    "success"
+  }
 
   /**
    * 删除指掌纹
@@ -40,7 +83,6 @@ class GatherFingerPalmServiceImpl extends GatherFingerPalmService{
   override def deleteFingerPalmData(person: String) (implicit session: DBSession) : Boolean = {
     try {
       deleteFingerData(person)
-      deletePalmData(person)
       true
     } catch {
       case exception : Exception => false
@@ -54,5 +96,6 @@ class GatherFingerPalmServiceImpl extends GatherFingerPalmService{
   def deletePalmData(personId: String) (implicit session: DBSession) = {
     withSQL { delete.from(GafisGatherPalm).where.eq(GafisGatherPalm.column.personId, personId) }.update.apply()
   }
+
 
 }
--- hall-api/src/main/scala/nirvana/hall/api/internal/stamp/GatherPortraitServiceImpl.scala
+++ hall-api/src/main/scala/nirvana/hall/api/internal/stamp/GatherPortraitServiceImpl.scala
@@ -1,12 +1,15 @@
 package nirvana.hall.api.internal.stamp
 
+import java.util
 import java.util.UUID
-
-import nirvana.hall.api.entities.GafisGatherPortrait._
-import nirvana.hall.api.entities.{GafisPerson, SysUser, GafisGatherPortrait}
+import javax.sql.rowset.serial.SerialBlob
+import nirvana.hall.api.entities.{GafisGatherPortrait}
+import nirvana.hall.api.internal.{AnalysisData}
 import nirvana.hall.api.services.stamp.GatherPortraitService
 import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 import scalikejdbc._
+
 
 /**
  * Created by wangjue on 2015/10/27.
@@ -24,43 +27,19 @@ class GatherPortraitServiceImpl extends GatherPortraitService{
     }.map(GafisGatherPortrait(gpt.resultName)).list.apply()
   }
 
-  /**
-   * 人像信息修改
-   * @param gafisGatherPortrait
-   * @return
-   */
-  override def updateGatherPortrait(gafisGatherPortrait: GafisGatherPortrait) (implicit session: DBSession = GafisGatherPortrait.autoSession) : Boolean = {
-    try {
-      withSQL {update(GafisPerson).set(
-        column.personid -> gafisGatherPortrait.personid,
-        column.fgp -> gafisGatherPortrait.fgp,
-        column.gatherData -> gafisGatherPortrait.gatherData,
-        column.inputpsn -> gafisGatherPortrait.inputpsn,
-        column.inputtime -> gafisGatherPortrait.inputtime,
-        column.modifiedpsn -> gafisGatherPortrait.modifiedpsn,
-        column.modifiedtime -> gafisGatherPortrait.modifiedtime,
-        column.deletag -> gafisGatherPortrait.deletag,
-        column.gatherdatanosqlid -> gafisGatherPortrait.gatherdatanosqlid
-      ).where.eq(GafisGatherPortrait.column.personid,gafisGatherPortrait.personid).and.eq(GafisGatherPortrait.column.fgp,gafisGatherPortrait.fgp)}.
-      update.apply()
-      true
-    } catch {
-      case exception: Exception => false
-    }
 
-  }
 
   /**
    * 人像信息新增
    * @param gafisGatherPortrait
    * @return
    */
-  override def addGatherPortrait(gafisGatherPortrait: GafisGatherPortrait, login: SysUser) (implicit session: DBSession = GafisGatherPortrait.autoSession) : Boolean = {
+  override def addGatherPortrait(gafisGatherPortrait: GafisGatherPortrait) (implicit session: DBSession = GafisGatherPortrait.autoSession) : Boolean = {
     try {
 
       GafisGatherPortrait.create(UUID.randomUUID().toString.replace("-",""),gafisGatherPortrait.personid,
-          gafisGatherPortrait.fgp,gafisGatherPortrait.gatherData,Some(login.pkId),
-            new DateTime(),Some(login.pkId),Some(new DateTime()),
+          gafisGatherPortrait.fgp,gafisGatherPortrait.gatherData,gafisGatherPortrait.inputpsn,
+            new DateTime(),gafisGatherPortrait.modifiedpsn,Some(new DateTime()),
               gafisGatherPortrait.deletag,gafisGatherPortrait.gatherdatanosqlid)
       true
     } catch {
@@ -68,4 +47,79 @@ class GatherPortraitServiceImpl extends GatherPortraitService{
     }
 
   }
+
+  /**
+   * 覆盖删除人像
+   * @param personid
+   * @param session
+   * @return
+   */
+  override def deleteGatherPortrait(personid: String)(implicit session: DBSession): Boolean = {
+    try {
+      withSQL {
+        delete.from(GafisGatherPortrait).where.eq(GafisGatherPortrait.column.personid,personid)
+      }
+      true
+    } catch {
+      case exception: Exception => false
+    }
+  }
+
+  /**
+   * 解析保存人像
+   * @param personId
+   * @param faceData
+   * @param session
+   * @return
+   */
+  override def analysisGatherPortrait(personId: String, faceData: String)(implicit session: DBSession): String = {
+    val listData : util.List[util.HashMap[_, _]] = AnalysisData.analysisPortrait(faceData)
+    deleteGatherPortrait(personId);//保存前删除人像
+    for (v <- 0 to listData.size()-1) {
+      val map : util.HashMap[_, _] = listData.get(v)
+      val pkId = map.get("pkid").toString
+      val fgp  = map.get("fgp").toString
+      val gatherData : SerialBlob = map.get("gatherData").asInstanceOf[SerialBlob]
+      val inputTime = DateTime.now()
+      GafisGatherPortrait.create(pkId,Some(personId),fgp,gatherData,None,inputTime,None,None,Some("1"),None)
+    }
+
+    /*val portraits : List[GafisGatherPortrait] = GafisGatherPortrait.findAllBy(sqls.eq(GafisGatherPortrait.column.personid,personId))
+    for (portrait <- portraits) {
+      val portrait = portraits(0)
+      val pkid = portrait.pkId
+      val fgp  = portrait.fgp
+      val gatherData  = portrait.gatherData
+      val inputTime = parseDateTimeToString(portrait.inputtime)
+      val by : Array[Byte] = gatherData.getBytes(0,gatherData.length().toInt)
+      val by1 : Array[Byte] = gatherData.getBytes(129,gatherData.length().toInt)
+      /*val out = new PrintWriter("C:\\image.jpg")
+      for (b <- by1) {
+        out.write(b)
+      }*/
+      println("gatherData---"+MsgBase64.toBase64(by1))
+      println("--inputTime--"+inputTime)
+    }*/
+
+    "true"
+  }
+
+  //格式化时间
+  def parseDateTime(time : String) : DateTime = {
+    if (time == None || time.equals("") || time.isEmpty)
+      null
+    else
+      DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss").parseDateTime(time)
+  }
+
+  def parseDateTimeToString(time : DateTime) : String = {
+    if (time == null)
+      ""
+    else
+      time.toString("yyyy-MM-dd HH:mm:ss")
+  }
+
+
 }
+
+
