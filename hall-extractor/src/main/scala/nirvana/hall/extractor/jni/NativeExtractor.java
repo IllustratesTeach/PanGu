b12ff099feca213f417f64a9947a4c8190c9bbbc
Jun Tsai <jcai@ganshane.com>
2015-12-10 23:21:01 +0800

    add doc

--- hall-extractor/src/main/scala/nirvana/hall/extractor/jni/NativeExtractor.java
+++ hall-extractor/src/main/scala/nirvana/hall/extractor/jni/NativeExtractor.java
@@ -7,7 +7,15 @@ package nirvana.hall.extractor.jni;
  */
 public class NativeExtractor{
     /**
-     * extract mnt
+     * extract feature
+     * @param img GAFISIMAGEHEADSTRUCT + image data
+     * @param mnt GAFISIMAGESTRUCT + mnt output
+     * @param finger_pos finger position
+     * @param ExtractMode extract mode. see afiskernel.EXETRACTMODE_XXX
+     * @param belatent 0 -> latent,1->template
      */
-    public static native void ExtractMNT_All(byte[] img,byte[] mnt,byte finger_pos,byte ExtractMode,byte belatent);
+    public static native void ExtractMNT_All(byte[] img,byte[] mnt,
+                                             byte finger_pos,
+                                             byte ExtractMode,
+                                             byte belatent);
 }
