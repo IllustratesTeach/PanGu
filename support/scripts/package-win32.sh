d13f9a34c634db3a5af3dd4d86ada3bf3db9cdfa
Jun Tsai <jcai@ganshane.com>
2015-12-11 13:30:22 +0800

    modify call

--- support/scripts/package-win32.sh
+++ support/scripts/package-win32.sh
@@ -1,8 +1,9 @@
 rm -rf build*
 export VERSION=`git log -n1 --pretty=format:%ai|cut -c1-10`
 echo $VERSION
-cmd /c support\\scripts\\compile-nmake.bat
+cmd /c support\\scripts\\compile-image.bat
 #scp build-w32/src/*.dll build-w32/src/*.lib jcai@dev.egfit.com:/opt/app/sites/lichen/nirvana/dll/w32
 #scp build-w64/src/*.dll build-w64/src/*.lib jcai@dev.egfit.com:/opt/app/sites/lichen/nirvana/dll/w64
+cmd /c support\\scripts\\compile-extractor.bat
 
 
