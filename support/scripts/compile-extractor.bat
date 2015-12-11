51d9f97b904eacf144115e285c0109ecc5ca24b7
Jun Tsai <jcai@ganshane.com>
2015-12-11 14:37:24 +0800

    Add win32 dll

--- support/scripts/compile-extractor.bat
+++ support/scripts/compile-extractor.bat
@@ -10,13 +10,14 @@ cd hall-extractor\c
 echo "start building with version %VERSION%"
 
 set ENV_PATH="c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+set KERNEL_DIR="C:\\Users\\jcai\\workspace\\nirvana-kernel"
 
 set ARCH=w32
 echo "Building %ARCH% .... "
 call %ENV_PATH%  x86
 mkdir build-%ARCH%
 cd build-%ARCH%
-cmake -DENABLE_JNI=on -DNIRVANA_SOURCE_VERSION=%VERSION% -DCMAKE_BUILD_TYPE=Release -DARCH=%ARCH% -DNIRVANA_NETWORK_LICENSE=on -G "NMake Makefiles" ..
+cmake -DKERNEL_DIR=%KERNEL_DIR% -DENABLE_JNI=on -DNIRVANA_SOURCE_VERSION=%VERSION% -DCMAKE_BUILD_TYPE=Release -DARCH=%ARCH% -DNIRVANA_NETWORK_LICENSE=on -G "NMake Makefiles" ..
 nmake
 cd ..
 
@@ -25,7 +26,7 @@ echo "Building %ARCH% ...."
 call %ENV_PATH%  amd64
 mkdir build-%ARCH%
 cd build-%ARCH%
-cmake -DENABLE_JNI=on -DNIRVANA_SOURCE_VERSION=%VERSION% -DCMAKE_BUILD_TYPE=Release -DARCH=%ARCH% -DNIRVANA_NETWORK_LICENSE=on -G "NMake Makefiles" ..
+cmake -DKERNEL_DIR=%KERNEL_DIR% -DENABLE_JNI=on -DNIRVANA_SOURCE_VERSION=%VERSION% -DCMAKE_BUILD_TYPE=Release -DARCH=%ARCH% -DNIRVANA_NETWORK_LICENSE=on -G "NMake Makefiles" ..
 nmake
 
 
