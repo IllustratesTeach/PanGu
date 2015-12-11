8f2b0705ad07659ac230dd08d2667f13df5e51c8
Jun Tsai <jcai@ganshane.com>
2015-12-11 22:31:57 +0800

    Add test extract feature from wsq file

--- hall-extractor/src/test/scala/nirvana/hall/extractor/internal/FeatureExtractorImplTest.scala
+++ hall-extractor/src/test/scala/nirvana/hall/extractor/internal/FeatureExtractorImplTest.scala
@@ -1,5 +1,6 @@
 package nirvana.hall.extractor.internal
 
+import nirvana.hall.c.services.gloclib.glocdef
 import nirvana.hall.c.services.gloclib.glocdef.GAFISIMAGESTRUCT
 import nirvana.hall.c.services.kernel.mnt_def.FINGERMNTSTRUCT
 import nirvana.hall.extractor.jni.BaseJniTest
@@ -14,6 +15,26 @@ import org.junit.Test
  * @since 2015-12-11
  */
 class FeatureExtractorImplTest extends BaseJniTest{
+  @Test
+  def test_extract_wsq: Unit ={
+    val img = IOUtils.toByteArray(getClass.getResourceAsStream("/wsq.data.uncompressed"))
+    val gafisImg = new GAFISIMAGESTRUCT
+    //gafisImg.fromByteArray(img1)
+    gafisImg.stHead.nHeight = 640
+    gafisImg.stHead.nWidth= 640
+    gafisImg.stHead.nResolution = 500
+    gafisImg.stHead.nImageType = glocdef.GAIMG_IMAGETYPE_FINGER.toByte
+    gafisImg.stHead.nImgSize = img.length
+    gafisImg.bnData = img
+
+    val extractor = new FeatureExtractorImpl
+    val mnt = extractor.extractByGAFISIMG(gafisImg,FingerPosition.FINGER_L_THUMB,FeatureType.Template)
+    val feature = new FINGERMNTSTRUCT
+    feature.fromByteArray(mnt.bnData)
+
+    feature
+
+  }
   @Test
   def test_extract: Unit ={
     val img = IOUtils.toByteArray(getClass.getResourceAsStream("/lf.img"))
