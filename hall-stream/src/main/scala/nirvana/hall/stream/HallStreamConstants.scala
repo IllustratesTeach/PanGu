c33c879f5b9da57db09e6886cf60d856f0cb8127
Jun Tsai <jcai@ganshane.com>
2015-12-15 19:29:49 +0800

    add constants variable

---
+++ hall-stream/src/main/scala/nirvana/hall/stream/HallStreamConstants.scala
@@ -0,0 +1,11 @@
+package nirvana.hall.stream
+
+/**
+ * hall stream constants
+ * @author <a href="mailto:jcai@ganshane.com">Jun Tsai</a>
+ * @since 2015-12-15
+ */
+object HallStreamConstants {
+  val PROTOBUF_HEADER = "X-Hall-Request"
+  val PROTOBUF_HEADER_VALUE = "ok"
+}
