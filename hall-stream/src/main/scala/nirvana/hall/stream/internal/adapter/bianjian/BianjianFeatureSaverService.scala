f8d93b9b2fd18d9f93f03579d36f76592970d5f6
songpeng <songpeng729@126.com>
2015-12-16 11:01:04 +0800

    读取数据实现

--- hall-stream/src/main/scala/nirvana/hall/stream/internal/adapter/bianjian/BianjianFeatureSaverService.scala
+++ hall-stream/src/main/scala/nirvana/hall/stream/internal/adapter/bianjian/BianjianFeatureSaverService.scala
@@ -1,5 +1,6 @@
 package nirvana.hall.stream.internal.adapter.bianjian
 
+import java.sql.{Connection, PreparedStatement}
 import javax.sql.DataSource
 
 import com.google.protobuf.ByteString
@@ -17,6 +18,14 @@ class BianjianFeatureSaverService(dataSource:DataSource) extends FeatureSaverSer
    * @param feature feature data
    */
   override def save(id: Any, feature: ByteString): Unit = {
-    throw new UnsupportedOperationException
+    val conn: Connection = dataSource.getConnection()
+    var ps: PreparedStatement = null
+    val sql: String = "insert into gafis_mnt (csid, mnt, seq) values(?,?,select FROM gafis_mnt_seq.nextval)"
+    ps = conn.prepareStatement(sql)
+    ps.setLong(1, id.asInstanceOf[Long])
+    ps.setBytes(2, feature.toByteArray)
+    ps.executeUpdate
+    ps.close()
+    conn.close()
   }
 }
--- hall-stream/src/main/scala/nirvana/hall/stream/internal/adapter/bianjian/BianjianStream.scala
+++ hall-stream/src/main/scala/nirvana/hall/stream/internal/adapter/bianjian/BianjianStream.scala
@@ -1,8 +1,12 @@
 package nirvana.hall.stream.internal.adapter.bianjian
 
+import java.sql.{SQLException, ResultSet, PreparedStatement, Connection}
 import javax.annotation.PostConstruct
 import javax.sql.DataSource
 
+import com.google.protobuf.ByteString
+import nirvana.hall.protocol.extract.ExtractProto.ExtractRequest.FeatureType
+import nirvana.hall.protocol.extract.ExtractProto.FingerPosition
 import nirvana.hall.stream.services.StreamService
 import org.apache.tapestry5.ioc.annotations.EagerLoad
 
@@ -13,13 +17,72 @@ import org.apache.tapestry5.ioc.annotations.EagerLoad
  */
 @EagerLoad
 class BianjianStream {
+  val batch_size = 10
 
   @PostConstruct
   def startStream(dataSource:DataSource,streamService: StreamService): Unit ={
-    //read data from databa source
-    //.........
+    val minSeq = getMinSeq(dataSource)
+    val maxSeq = getMaxSeq(dataSource)
+    fetch(dataSource, streamService, batch_size, minSeq, maxSeq)
+  }
+  //递归
+  def fetch(dataSource:DataSource,streamService: StreamService, size: Int, from: Long, maxSeq: Long): Unit ={
+    if(from < maxSeq){
+      fetchData(dataSource, streamService, size, from)
+      fetch(dataSource, streamService, size, from + batch_size, maxSeq)
+    }
+  }
+  def fetchData(dataSource:DataSource,streamService: StreamService, size: Int, from: Long): Unit ={
+    val conn = dataSource.getConnection
+    val ps = conn.prepareStatement("select t.csid, t.zp from T_PC_A_CS t where t.csid >? and t.csid <=? order by t.csid")
+    ps.setLong(1, from)
+    ps.setLong(2, from + size)
+    val rs = ps.executeQuery()
 
-    //push data
-    //streamService.pushEvent()
+    readAndPush(rs, streamService)
+
+    rs.close()
+    ps.close()
+    conn.close()
+  }
+
+  def readAndPush(rs: ResultSet, streamService: StreamService): Unit ={
+    if(rs.next()){
+      val csid = rs.getLong("csid")
+      val zp = rs.getBytes("zp")
+      streamService.pushEvent(csid, ByteString.copyFrom(zp), true, FingerPosition.FINGER_L_THUMB, FeatureType.FingerTemplate)
+      readAndPush(rs, streamService)
+    }
+  }
+
+
+  def getMaxSeq (dataSource: DataSource): Long ={
+    getSeq(dataSource, "select min(csid) from T_PC_A_CS")
+  }
+  def getMinSeq(dataSource: DataSource): Long={
+    getSeq(dataSource, "select max(csid) from T_PC_A_CS")
+  }
+  def getSeq(dataSource: DataSource, sql: String): Long ={
+    val conn: Connection = dataSource.getConnection
+    var ps: PreparedStatement = null
+    var rs: ResultSet = null
+    var seq: Long = 0
+    try {
+      ps = conn.prepareStatement(sql)
+      rs = ps.executeQuery
+      if (rs.next) {
+        seq = rs.getLong(1)
+      }
+    }
+    catch {
+      case e: SQLException => {
+        e.printStackTrace
+      }
+    } finally {
+      rs.close()
+      ps.close()
+      conn.close()
+    }
+    seq
   }
 }
