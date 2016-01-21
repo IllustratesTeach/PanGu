00b131c2790b17ed6a36102e4bfe8d7a70f31e91
Jun Tsai <jcai@ganshane.com>
2016-01-21 21:03:24 +0800

    fix test case

--- hall-stream/src/test/scala/nirvana/hall/stream/internal/HttpExtractServiceTest.scala
+++ hall-stream/src/test/scala/nirvana/hall/stream/internal/HttpExtractServiceTest.scala
@@ -20,14 +20,20 @@ class HttpExtractServiceTest{
     val registry = ExtensionRegistry.newInstance()
     ExtractProto.registerAllExtensions(registry)
     val httpClient = new RpcHttpClientImpl(registry)
-    val service = new HttpExtractService("http://127.0.0.1:9002/extractor", httpClient,new HallStreamConfig)
+    val config = new HallStreamConfig
+    config.isNewFeature = true
+    val service = new HttpExtractService("http://10.1.7.144:9002/extractor", httpClient,config)
     val is = getClass.getResourceAsStream("/wsq.data.uncompressed")
     val img = ByteString.readFrom(is).toByteArray
     val gafisImg = new GAFISIMAGESTRUCT
     gafisImg.stHead.nImageType = glocdef.GAIMG_IMAGETYPE_FINGER.toByte
     gafisImg.stHead.nImgSize = img.length
-    gafisImg.stHead.bIsCompressed = 1
-    gafisImg.stHead.nCompressMethod = glocdef.GAIMG_CPRMETHOD_WSQ.toByte
+    gafisImg.stHead.nWidth = 640
+    gafisImg.stHead.nBits= 8
+    gafisImg.stHead.nResolution = 500
+    gafisImg.stHead.nHeight= 640
+    gafisImg.stHead.bIsCompressed = 0
+    gafisImg.stHead.nCompressMethod = 0
     gafisImg.bnData = img
 
     val mnt = service.extract(gafisImg, FingerPosition.FINGER_L_THUMB, FeatureType.FingerLatent)
