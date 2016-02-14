55e096046f879fe334d9f4cb4cb90a52aa6fa021
Jun Tsai <jcai@ganshane.com>
2016-02-14 14:20:57 +0800

    Add download method to download file

--- hall-support/src/main/scala/nirvana/hall/support/internal/RpcHttpClientImpl.scala
+++ hall-support/src/main/scala/nirvana/hall/support/internal/RpcHttpClientImpl.scala
@@ -2,18 +2,19 @@
 // site: http://www.ganshane.com
 package nirvana.hall.support.internal
 
-import java.io.{Closeable, IOException}
+import java.io.Closeable
 
 import com.google.protobuf.ExtensionRegistry
 import com.google.protobuf.GeneratedMessage.GeneratedExtension
 import monad.rpc.protocol.CommandProto.BaseCommand
 import nirvana.hall.support.HallSupportConstants
 import nirvana.hall.support.services.RpcHttpClient
+import org.apache.commons.io.IOUtils
 import org.apache.http.HttpEntity
 import org.apache.http.client.config.RequestConfig
-import org.apache.http.client.methods.{CloseableHttpResponse, HttpPost}
+import org.apache.http.client.methods.{CloseableHttpResponse, HttpGet, HttpPost}
 import org.apache.http.entity.ByteArrayEntity
-import org.apache.http.impl.client.{HttpClientBuilder, CloseableHttpClient}
+import org.apache.http.impl.client.{CloseableHttpClient, HttpClientBuilder}
 import org.apache.http.util.EntityUtils
 
 import scala.util.control.NonFatal
@@ -23,6 +24,37 @@ import scala.util.control.NonFatal
  * @author jcai
  */
 class RpcHttpClientImpl(extensionRegistry: ExtensionRegistry) extends RpcHttpClient{
+  def download(url:String): Array[Byte] ={
+    val httpClient: CloseableHttpClient = createHttpClient
+    try {
+      val get = new HttpGet(url)
+      val response: CloseableHttpResponse = httpClient.execute(get)
+      try {
+        if (response.getStatusLine.getStatusCode == 200) {
+          val entity = response.getEntity
+          try {
+            IOUtils.toByteArray(entity.getContent)
+          } finally {
+            EntityUtils.consume(entity)
+          }
+        }
+        else throw new CallRpcException(response.getStatusLine.toString)
+      }
+      finally {
+        close(response)
+      }
+    }
+    catch {
+      case e:CallRpcException =>
+        throw e
+      case NonFatal(e)=>
+        throw new CallRpcException(e.getMessage)
+
+    }
+    finally {
+      close(httpClient)
+    }
+  }
   /**
    * 通过protobuf的数据来调用远程的url
    * @param url web application url
@@ -50,15 +82,17 @@ class RpcHttpClientImpl(extensionRegistry: ExtensionRegistry) extends RpcHttpCli
             EntityUtils.consume(entity)
           }
         }
-        else throw new RuntimeException(response.getStatusLine.toString)
+        else throw new CallRpcException(response.getStatusLine.toString)
       }
       finally {
         close(response)
       }
     }
     catch {
-      case e: IOException =>
-        throw new RuntimeException(e)
+      case e:CallRpcException =>
+        throw e
+      case NonFatal(e)=>
+        throw new CallRpcException(e.getMessage)
 
     }
     finally {
@@ -84,4 +118,5 @@ class RpcHttpClientImpl(extensionRegistry: ExtensionRegistry) extends RpcHttpCli
     HttpClientBuilder.create.setDefaultRequestConfig(defaultConfig).setUserAgent("nirvana-hall-stream/1.0").build
   }
 }
+class CallRpcException(message:String) extends RuntimeException(message)
 
