4cfeee1d85ebbb176ca86d8606a24dacbc7ed5dc
Jun Tsai <jcai@ganshane.com>
2016-02-16 13:35:17 +0800

    add supervisor application

--- hall-image/src/main/scala/nirvana/hall/image/app/HallImageSupervisorApp.scala
+++ hall-image/src/main/scala/nirvana/hall/image/app/HallImageSupervisorApp.scala
@@ -1,4 +1,4 @@
-package nirvana.hall.image.internal
+package nirvana.hall.image.app
 
 import java.io.{IOException, InputStream, OutputStream}
 import java.lang.ProcessBuilder.Redirect
@@ -9,20 +9,21 @@ import scala.collection.JavaConversions._
 import scala.collection.mutable.ArrayBuffer
 
 /**
- *
+ * supervise hall image app
  * @author <a href="mailto:jcai@ganshane.com">Jun Tsai</a>
  * @since 2016-02-15
  */
-object DecompressProcess extends LoggerSupport{
-  def main(args:Array[String]): Unit ={
-    while(true)
+object HallImageSupervisorApp extends LoggerSupport {
+  def main(args: Array[String]): Unit = {
+    while (true)
       startDecompressProcess()
   }
-  def startDecompressProcess(): Unit ={
-    val javaBinary = System.getProperty("java.home")+"/bin/java"
+
+  def startDecompressProcess(): Unit = {
+    val javaBinary = System.getProperty("java.home") + "/bin/java"
     val classPath = System.getProperty("java.class.path")
     // Create and start the worker
-    val properties = System.getProperties.map{case (k,v)=>"-D%s=%s".format(k,v)}
+    val properties = System.getProperties.map { case (k, v) => "-D%s=%s".format(k, v) }
     val commandParameters = new ArrayBuffer[String]()
     commandParameters += javaBinary
     commandParameters.appendAll(properties)
@@ -32,14 +33,14 @@ object DecompressProcess extends LoggerSupport{
     commandParameters += "nirvana.hall.image.app.HallImageApp"
     //"nirvana.hall.image.internal.FirmMain"
 
-    val pb= new ProcessBuilder(commandParameters :_*)
+    val pb = new ProcessBuilder(commandParameters: _*)
     val workerEnv = pb.environment()
     pb.redirectErrorStream(true)
     pb.redirectOutput(Redirect.PIPE)
     val worker = pb.start()
     // Redirect worker stdout and stderr
     redirectStreamsToStderr(worker.getInputStream, worker.getErrorStream)
-    sys.addShutdownHook{
+    sys.addShutdownHook {
       worker.destroy()
     }
 
@@ -58,14 +59,15 @@ object DecompressProcess extends LoggerSupport{
         logger.error("Exception in redirecting streams", e)
     }
   }
-  private class RedirectThread(
-                                       in: InputStream,
-                                       out: OutputStream,
-                                       name: String,
-                                       propagateEof: Boolean = false)
+
+  private class RedirectThread( in: InputStream,
+                                out: OutputStream,
+                                name: String,
+                                propagateEof: Boolean = false)
     extends Thread(name) {
 
     setDaemon(true)
+
     override def run() {
       scala.util.control.Exception.ignoring(classOf[IOException]) {
         // FIXME: We copy the stream on the level of bytes to avoid encoding problems.
@@ -85,6 +87,7 @@ object DecompressProcess extends LoggerSupport{
       }
     }
   }
+
   def tryWithSafeFinally[T](block: => T)(finallyBlock: => Unit): T = {
     // It would be nice to find a method on Try that did this
     var originalThrowable: Throwable = null
--- hall-stream/src/test/scala/nirvana/hall/stream/internal/HttpDecompressServiceTest.scala
+++ hall-stream/src/test/scala/nirvana/hall/stream/internal/HttpDecompressServiceTest.scala
@@ -4,6 +4,8 @@ import java.io.{File, FileInputStream}
 import java.util.concurrent.{CountDownLatch, Executors}
 
 import com.google.protobuf.{ByteString, ExtensionRegistry}
+import nirvana.hall.c.services.gfpt4lib.FPTFile
+import nirvana.hall.c.services.gfpt4lib.FPTFile.FPTParseException
 import nirvana.hall.c.services.gloclib.glocdef
 import nirvana.hall.c.services.gloclib.glocdef.GAFISIMAGESTRUCT
 import nirvana.hall.protocol.image.FirmImageDecompressProto
@@ -16,6 +18,125 @@ import org.junit.{Assert, Test}
  * Created by songpeng on 15/12/15.
  */
 class HttpDecompressServiceTest {
+
+  @Test
+  def test_performance: Unit ={
+    //LocalRpcModule.buildProtobufRegistroy(config)
+    val registry = ExtensionRegistry.newInstance()
+    FirmImageDecompressProto.registerAllExtensions(registry)
+    val httpClient = new RpcHttpClientImpl(registry)
+
+    val service = new HttpDecompressService("http://127.0.0.1:9999/image",httpClient)
+    val executor = Executors.newFixedThreadPool(5)
+
+
+
+    val files = FileUtils.listFiles(new File("/Users/jcai/Downloads/fpt-files/2/error"),Array[String]("fpt","FPT","fptt"),true)
+    val it = files.iterator()
+    while(it.hasNext){
+      val file = it.next()
+      //      println("processing "+file.getAbsolutePath)
+      try {
+        val is = new FileInputStream(file)
+        val result = FPTFile.parseFromInputStream(is)
+        IOUtils.closeQuietly(is)
+
+        /*
+    gafisImg.stHead.bIsCompressed = 1
+    gafisImg.stHead.nCompressMethod = glocdef.GAIMG_CPRMETHOD_WSQ.toByte
+    gafisImg.bnData = bnData
+    gafisImg.stHead.nImgSize = bnData.length
+    */
+
+        //val result = service.decompress(request.build())
+
+        var method:String = null
+        result match{
+          case Right(fpt4)=>
+            assert(fpt4.fileLength.toInt == fpt4.getDataSize)
+            val tpCount = fpt4.tpCount.toInt
+            if(tpCount > 0){
+              assert(tpCount == fpt4.logic02Recs.size)
+              fpt4.logic02Recs.foreach{tp=>
+                val fingerCount = tp.sendFingerCount.toInt
+                assert(fingerCount == tp.fingers.size)
+                tp.fingers.foreach{tData=>
+                  method = tData.imgCompressMethod
+                  //println("compress method "+tData.imgCompressMethod)
+                  if(method == "1700"){
+                    val gafisImg = new GAFISIMAGESTRUCT
+                    gafisImg.stHead.nWidth = tData.imgHorizontalLength.toShort
+                    gafisImg.stHead.nHeight = tData.imgVerticalLength.toShort
+                    gafisImg.stHead.bIsCompressed = 1
+                    gafisImg.stHead.nCompressMethod= glocdef.GAIMG_CPRMETHOD_COGENT
+                    gafisImg.stHead.nImgSize = tData.imgDataLength.toInt
+                    gafisImg.stHead.nBits = 8
+
+                    gafisImg.bnData = tData.imgData
+
+                    if(gafisImg.stHead.nImgSize == 0) {
+                      println("image is zero")
+                    }else {
+                      executor.execute{new Runnable {
+                        override def run(): Unit = {
+                          print(method + " " + file.getAbsolutePath+" ")
+                          val request = FirmImageDecompressRequest.newBuilder()
+                          request.setCprData(ByteString.copyFrom(gafisImg.toByteArray))
+                          service.decompress(request.build())
+                          println("ok")
+                        }
+                      }}
+                    }
+
+                  }
+                }
+              }
+            }
+          case Left(fpt3) =>
+            val tpCount = fpt3.tpCount.toInt
+            if(tpCount >0) {
+              var method:String = null
+              assert(fpt3.tpCount.toInt == fpt3.logic3Recs.size)
+              fpt3.logic3Recs.foreach{tp=>
+                val fingerCount = tp.sendFingerCount.toInt
+                assert(fingerCount == tp.fingers.size)
+                tp.fingers.foreach{tData=>
+                  method = tData.imgCompressMethod
+                  //println("compress method "+tData.imgCompressMethod)
+                  if(method == "1700"){
+                    val gafisImg = new GAFISIMAGESTRUCT
+                    gafisImg.stHead.bIsCompressed = 1
+                    gafisImg.stHead.nCompressMethod= glocdef.GAIMG_CPRMETHOD_COGENT
+                    gafisImg.stHead.nImgSize = tData.imgDataLength.toInt
+                    gafisImg.stHead.nBits = 8
+
+                    gafisImg.bnData = tData.imgData
+
+                    println(method + " " + file.getAbsolutePath)
+                    executor.execute(new Runnable {
+                      override def run(): Unit = {
+                        print(method + " " + file.getAbsolutePath+" ")
+                        val request = FirmImageDecompressRequest.newBuilder()
+                        request.setCprData(ByteString.copyFrom(gafisImg.toByteArray))
+                        service.decompress(request.build())
+                      }
+                    })
+
+                  }
+                }
+              }
+            }
+        }
+
+      }catch{
+        case e:FPTParseException =>
+          //do nothing
+        case e:Throwable=>
+          e.printStackTrace()
+        //          println(file.getAbsolutePath+" \n\te:"+e.toString)
+      }
+    }
+  }
   @Test
   def test_daiku: Unit ={
     //LocalRpcModule.buildProtobufRegistroy(config)
