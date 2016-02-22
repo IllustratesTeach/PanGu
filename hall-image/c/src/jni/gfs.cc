8697de6c01da6c6a613eebe9af2455eb0dd0d535
Jun Tsai <jcai@ganshane.com>
2016-02-22 14:16:54 +0800

    use direct decompress method

--- hall-image/c/src/jni/gfs.cc
+++ hall-image/c/src/jni/gfs.cc
@@ -7,15 +7,15 @@
 JNIEXPORT void JNICALL Java_nirvana_hall_image_jni_NativeImageConverter_decodeByGFS
    (JNIEnv *jenv, jclass, jbyteArray compressed_img, jbyteArray original_data){
 
-  int compressed_size = jenv->GetArrayLength(compressed_img);
   UCHAR* compressed_img_bin = (UCHAR*)jenv->GetByteArrayElements(compressed_img, JNI_FALSE);
   GAFISIMAGESTRUCT* cpr =(GAFISIMAGESTRUCT*)(compressed_img_bin);
+  FPT_datatranform(cpr);
 
-  int original_size = jenv->GetArrayLength(original_data);
   UCHAR* original_img_bin = (UCHAR*)jenv->GetByteArrayElements(original_data, JNI_FALSE);
   GAFISIMAGESTRUCT* original_img =(GAFISIMAGESTRUCT*)(original_img_bin);
 
   int ret = GAFIS_DecompressIMG(cpr,original_img);
+
   //release compressed image
   jenv->ReleaseByteArrayElements(compressed_img, (jbyte *) compressed_img_bin, JNI_ABORT);
   jenv->ReleaseByteArrayElements(original_data, (jbyte *) original_img_bin, 0);
--- hall-image/src/test/scala/nirvana/hall/image/jni/NativeImageConverterTest.scala
+++ hall-image/src/test/scala/nirvana/hall/image/jni/NativeImageConverterTest.scala
@@ -1,6 +1,10 @@
 package nirvana.hall.image.jni
 
+import javax.imageio.spi.IIORegistry
+
+import nirvana.hall.c.services.gfpt4lib.FPTFile
 import nirvana.hall.c.services.gloclib.glocdef.GAFISIMAGESTRUCT
+import nirvana.hall.support.services.GAFISImageReaderSpi
 import org.apache.commons.io.IOUtils
 import org.junit.{Assert, Test}
 
@@ -10,18 +14,43 @@ import org.junit.{Assert, Test}
  * @since 2015-12-10
  */
 class NativeImageConverterTest extends BaseJniTest{
+  val iioRegistry = IIORegistry.getDefaultInstance
+  iioRegistry.registerServiceProvider(new GAFISImageReaderSpi)
+
   @Test
   def test_gfs: Unit ={
-    val is = getClass.getResourceAsStream("/lf.img")
-    val gafisImg = new GAFISIMAGESTRUCT
-    gafisImg.fromStreamReader(is)
+    var seq=0;
+    val fptOpt=FPTFile.parseFromInputStream(getClass.getResourceAsStream("/gfs.fpt"))
+    fptOpt match{
+      case Left(fpt3)=>
+        fpt3.logic3Recs.foreach{rec=>
+          rec.fingers.foreach{f=>
+            val method = f.imgCompressMethod
+            val img=f.imgData
+            val width =f.imgHorizontalLength
+            val height = f.imgVerticalLength
+            val gafisImg = new GAFISIMAGESTRUCT
+            gafisImg.fromByteArray(img)
 
-    val destImg = new GAFISIMAGESTRUCT
-    destImg.stHead.fromByteArray(gafisImg.stHead.toByteArray)
-    destImg.stHead.nImgSize = destImg.stHead.nWidth * destImg.stHead.nHeight
-    destImg.bnData = new Array[Byte](destImg.stHead.nImgSize)
+            /*
+            val destImg = new GAFISIMAGESTRUCT
+            destImg.stHead.fromByteArray(gafisImg.stHead.toByteArray)
+            destImg.stHead.nImgSize = destImg.stHead.nWidth * destImg.stHead.nHeight
+            destImg.bnData = new Array[Byte](destImg.stHead.nImgSize)
+            */
 
-    NativeImageConverter.decodeByGFS(gafisImg.toByteArray,destImg.toByteArray)
+            val destImgBytes = new Array[Byte](gafisImg.stHead.getDataSize + gafisImg.stHead.nWidth * gafisImg.stHead.nHeight)
+            NativeImageConverter.decodeByGFS(gafisImg.toByteArray,destImgBytes)
+            /*
+            val destImgBmp= ImageIO.read(new ByteArrayInputStream(destImgBytes))
+            seq +=1
+            ImageIO.write(destImgBmp, "bmp", new File(seq+".bmp"));
+            */
+          }
+        }
+      case Right(fpt4)=>
+    }
+
   }
   @Test
   def test_wsq: Unit ={
