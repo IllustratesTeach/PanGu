c2646aa5568c1cb067ed056d17de24887e42cb12
Jun Tsai <jcai@ganshane.com>
2016-04-11 15:49:53 +0800

    add fpt3 test case

--- hall-c/src/test/scala/nirvana/hall/c/services/kernel/FPTLDataToMNTDISPTest.scala
+++ hall-c/src/test/scala/nirvana/hall/c/services/kernel/FPTLDataToMNTDISPTest.scala
@@ -3,6 +3,7 @@ package nirvana.hall.c.services.kernel
 import java.io.{File, FileInputStream}
 
 import nirvana.hall.c.AncientConstants
+import nirvana.hall.c.services.gfpt4lib.FPT3File.FPT3File
 import nirvana.hall.c.services.gfpt4lib.FPT4File.FPT4File
 import org.junit.Test
 
@@ -18,6 +19,16 @@ class FPTLDataToMNTDISPTest {
   fpt4.fromStreamReader(new FileInputStream(new File("/Users/jcai/Downloads/A6500000009992100050002.FPT")),AncientConstants.GBK_ENCODING  )
 
   val headL = fpt4.logic03Recs.head.fingers.head
+  val disp = FPTLDataToMNTDISP.convertFPT03ToMNTDISP(headL)
+
+  val palm = disp.bIsPalm
+ }
+ @Test
+ def test_disp_fpt3: Unit ={
+  val fpt3 = new FPT3File
+  fpt3.fromStreamReader(new FileInputStream(new File("/Users/jcai/Downloads/A2342124324222297342355.fpt")),AncientConstants.GBK_ENCODING  )
+
+  val headL = fpt3.logic2Recs.head.fingers.head
   val disp = FPTLDataToMNTDISP.convertFPT03ToMNTDISP(headL)
 
   val palm = disp.bIsPalm
