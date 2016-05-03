081aadb2c9b1f539e03556446cfd3d088ad277b1
Jun Tsai <jcai@ganshane.com>
2016-05-03 16:27:10 +0800

    添加能够收取包的方法

--- hall-api/src/main/scala/nirvana/hall/v62/internal/c/grmtlib/grmtcsr.scala
+++ hall-api/src/main/scala/nirvana/hall/v62/internal/c/grmtlib/grmtcsr.scala
@@ -6,6 +6,7 @@ import nirvana.hall.v62.internal.c.gnetlib.{gnetcsr, reqansop}
 
 /**
   * 远程通信服务器的客户端程序
+  *
   * @author <a href="mailto:jcai@ganshane.com">Jun Tsai</a>
   * @since 2016-04-27
   */
@@ -23,6 +24,37 @@ trait grmtcsr {
       }
       */
       NETOP_SENDDATA(channelOperator,pstPkg)
+    }
+  }
+
+  def GAFIS_RMTLIB_RecvPkg(pbIsProxyReq:Boolean=false) {
+    executeInChannel { channelOperator =>
+
+      val channelBuffer = NETOP_RECVDATA(channelOperator, 4)
+      channelBuffer.markReaderIndex()
+      val bytes = channelBuffer.readBytes(4).array()
+      channelBuffer.resetReaderIndex()
+      val dataLength = channelBuffer.getInt()
+
+      /*
+    //for normal net function, only for OP_CLASS_FILE and OP_CLASS_HOTUPDATE
+    if(Char4To_uint4(nDataLen) == sizeof(GNETREQUESTHEADOBJECT))	//normal net function
+    {
+      NETOP_RECVDATA(pstCon, (UCHAR*)&stReq + sizeof(stReq.cbSize),sizeof(stReq)-sizeof(stReq.cbSize));
+      CHAR4COPY(stReq.cbSize,nDataLen);
+      if( GBASE_ITEMPKG_New(pstPkg,1024) < 0 ) ERRFAILFINISHEXIT();
+      if( GAFIS_PKG_AddRmtRequest(pstPkg,&stReq) < 0 ) ERRFAILFINISHEXIT();
+      retval = pstPkg->nPkgLen;
+      if( pbIsProxyReq )	*pbIsProxyReq = 1;
+      goto Finish_Exit;
+    }
+    */
+      val pstPkg = new GBASE_ITEMPKG_OPSTRUCT
+      pstPkg.head.nDataLen = dataLength
+
+      NETOP_SENDDATA(channelOperator, bytes)
+      NETOP_RECVDATA(channelBuffer, pstPkg)
+
     }
   }
 }
