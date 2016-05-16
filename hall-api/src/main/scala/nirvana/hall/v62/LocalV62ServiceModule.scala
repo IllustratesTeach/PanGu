303b5acf1fdeb3a1d71651a9408923bba46f8007
Jun Tsai <jcai@ganshane.com>
2016-05-16 11:45:08 +0800

    完美解决动态服务器地址的支持，能够在运行时刻调整服务器的地址信息

--- hall-api/src/main/scala/nirvana/hall/v62/LocalV62ServiceModule.scala
+++ hall-api/src/main/scala/nirvana/hall/v62/LocalV62ServiceModule.scala
@@ -3,8 +3,11 @@ package nirvana.hall.v62
 import nirvana.hall.api.services._
 import nirvana.hall.support.internal.RpcHttpClientImpl
 import nirvana.hall.support.services.RpcHttpClient
+import nirvana.hall.v62.internal.V62Facade.AutoSetupServerContextFilter
 import nirvana.hall.v62.internal._
-import org.apache.tapestry5.ioc.ServiceBinder
+import org.apache.tapestry5.ioc.annotations.Contribute
+import org.apache.tapestry5.ioc.{OrderedConfiguration, ServiceBinder}
+import org.apache.tapestry5.services.{HttpServletRequestFilter, HttpServletRequestHandler}
 
 /**
  * local v62 service module
@@ -18,6 +21,9 @@ object LocalV62ServiceModule {
     binder.bind(classOf[LPCardService], classOf[LPCardServiceImpl])
     binder.bind(classOf[CaseInfoService], classOf[CaseInfoServiceImpl])
     binder.bind(classOf[QueryService], classOf[QueryServiceImpl])
-
+  }
+  @Contribute(classOf[HttpServletRequestHandler])
+  def provideAutoSetupServerContextFilter(configuration: OrderedConfiguration[HttpServletRequestFilter]): Unit = {
+    configuration.addInstance("AutoSetupServerContext", classOf[AutoSetupServerContextFilter], "before:protobuf")
   }
 }
--- hall-api/src/main/scala/nirvana/hall/v62/internal/V62Facade.scala
+++ hall-api/src/main/scala/nirvana/hall/v62/internal/V62Facade.scala
@@ -1,17 +1,21 @@
 package nirvana.hall.v62.internal
 
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
+
 import monad.support.services.LoggerSupport
 import nirvana.hall.v62.config.HallV62Config
 import nirvana.hall.v62.internal.c.gnetlib._
 import nirvana.hall.v62.internal.c.grmtlib.gnetfunc
 import nirvana.hall.v62.services.V62ServerAddress
+import org.apache.tapestry5.services.{HttpServletRequestFilter, HttpServletRequestHandler}
 
 /**
  * v62 facade
- * @author <a href="mailto:jcai@ganshane.com">Jun Tsai</a>
+  *
+  * @author <a href="mailto:jcai@ganshane.com">Jun Tsai</a>
  * @since 2015-11-04
  */
-class V62Facade(config:HallV62Config)
+class V62Facade
   extends gnetcsr
   with DataSyncSupport
   with ganetqry
@@ -24,7 +28,37 @@ class V62Facade(config:HallV62Config)
   with AncientClientSupport
   with reqansop
   with LoggerSupport{
-  private val address = V62ServerAddress(config.host,config.port,
-    config.connectionTimeoutSecs,config.readTimeoutSecs,config.user,Option(config.password))
-  override def serverAddress: V62ServerAddress = address
+  override def serverAddress: V62ServerAddress = {
+    val address = V62Facade.serverContext.value
+    if(address == null)
+      throw new IllegalStateException("can't find v62 server address from context")
+  }
+}
+object V62Facade{
+  /**
+    * 动态获取服务器的地址，通过动态变量的上下文进行获取
+    */
+  private[v62] val serverContext = new scala.util.DynamicVariable[V62ServerAddress](null)
+  /**
+    * 针对v62的服务器端请求，自动添加请求的上下文
+    * NOTICE: 此filter的位置需要放置在protobuf的前端
+    * @see ProtobufServletFilter
+    * @see LocalV62ServiceModule
+    * @see LocalApiWebModule
+    */
+  class AutoSetupServerContextFilter(config:HallV62Config) extends HttpServletRequestFilter {
+    //服务器的地址配置
+    private val address = V62ServerAddress(config.host,
+      config.port,
+      config.connectionTimeoutSecs,
+      config.readTimeoutSecs,
+      config.user,
+      Option(config.password))
+
+    override def service(request: HttpServletRequest, response: HttpServletResponse, handler: HttpServletRequestHandler): Boolean = {
+      serverContext.withValue(address){
+        handler.service(request,response)
+      }
+    }
+  }
 }
