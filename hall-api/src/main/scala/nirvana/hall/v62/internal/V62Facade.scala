0810b3f3fa4cb6efca8c547f43fbb8612e6f927c
Jun Tsai <jcai@ganshane.com>
2016-05-17 15:21:53 +0800

    动态根据http请求的头信息来连接对应的应用服务器

--- hall-api/src/main/scala/nirvana/hall/v62/internal/V62Facade.scala
+++ hall-api/src/main/scala/nirvana/hall/v62/internal/V62Facade.scala
@@ -38,6 +38,12 @@ class V62Facade
   }
 }
 object V62Facade{
+  //http请求头标示
+  val X_V62_HOST_HEAD     ="X-V62-HOST"
+  val X_V62_PORT_HEAD     ="X-V62-PORT"
+  val X_V62_USER_HEAD     ="X-V62-USER"
+  val X_V62_PASSWORD_HEAD ="X-V62-PASS"
+
   /**
     * 动态获取服务器的地址，通过动态变量的上下文进行获取
     */
@@ -45,6 +51,8 @@ object V62Facade{
   /**
     * 针对v62的服务器端请求，自动添加请求的上下文
     * NOTICE: 此filter的位置需要放置在protobuf的前端
+    *
+    * 如果前端的请求指定了服务器相关信息，则采用指定的信息进行连接
     * @see ProtobufServletFilter
     * @see LocalV62ServiceModule
     * @see LocalApiWebModule
@@ -59,9 +67,23 @@ object V62Facade{
       Option(config.password))
 
     override def service(request: HttpServletRequest, response: HttpServletResponse, handler: HttpServletRequestHandler): Boolean = {
-      serverContext.withValue(address){
+      val host = request.getHeader(X_V62_HOST_HEAD)
+      val v62Address =
+        if(host == null){
+          address
+        }else{
+          val port = getHeader(request,X_V62_PORT_HEAD,config.port.toString).toInt
+          val user = getHeader(request,X_V62_USER_HEAD,config.user)
+          val passOpt = Option(getHeader(request,X_V62_USER_HEAD,config.password))
+          V62ServerAddress(host,port,config.connectionTimeoutSecs,config.readTimeoutSecs,user,passOpt)
+        }
+      serverContext.withValue(v62Address){
         handler.service(request,response)
       }
+    }
+    private def getHeader(request:HttpServletRequest,name:String,defaultValue:String):String={
+      val value = request.getHeader(name)
+      if(value == null ) defaultValue else value
     }
   }
 }
