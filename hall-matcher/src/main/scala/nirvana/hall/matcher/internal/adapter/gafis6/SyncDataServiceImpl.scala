c598f08ff2bc925b3dcaa06b3d4d27815223ab36
Jun Tsai <jcai@ganshane.com>
2016-06-04 09:05:22 +0800

    添加数据量的输出信息

--- hall-matcher/src/main/scala/nirvana/hall/matcher/internal/adapter/gafis6/SyncDataServiceImpl.scala
+++ hall-matcher/src/main/scala/nirvana/hall/matcher/internal/adapter/gafis6/SyncDataServiceImpl.scala
@@ -2,6 +2,7 @@ package nirvana.hall.matcher.internal.adapter.gafis6
 
 import javax.sql.DataSource
 
+import monad.support.services.LoggerSupport
 import nirvana.hall.matcher.config.HallMatcherConfig
 import nirvana.hall.matcher.service.SyncDataService
 import nirvana.protocol.NirvanaTypeDefinition.SyncDataType
@@ -10,7 +11,9 @@ import nirvana.protocol.SyncDataProto.{SyncDataRequest, SyncDataResponse}
 /**
  * gafis6.2分库service
  */
-class SyncDataServiceImpl(hallMatcherConfig: HallMatcherConfig, dataSource: DataSource) extends SyncDataService{
+class SyncDataServiceImpl(hallMatcherConfig: HallMatcherConfig, dataSource: DataSource)
+  extends SyncDataService
+  with LoggerSupport{
   /**
    * 同步数据
     *
@@ -23,6 +26,7 @@ class SyncDataServiceImpl(hallMatcherConfig: HallMatcherConfig, dataSource: Data
     val size = syncDataRequest.getSize
     val timestamp = syncDataRequest.getTimestamp
     val syncDataType = syncDataRequest.getSyncDataType
+    info("fetching data timestamp:{} size:{}",timestamp,size)
     val fetcher = syncDataType match {
 //      case SyncDataType.PERSON => new sync.PersonFetcher(hallMatcherConfig, dataSource)
 //      case SyncDataType.CASE => new sync.CaseFetcher(hallMatcherConfig, dataSource)
@@ -34,6 +38,7 @@ class SyncDataServiceImpl(hallMatcherConfig: HallMatcherConfig, dataSource: Data
     }
     if(fetcher != null)
       fetcher.doFetch(responseBuilder, size, timestamp)
+    info("{} data fetched with timestamp:{} size:{}",responseBuilder.getSyncDataCount,timestamp,size)
     responseBuilder.build()
   }
 
