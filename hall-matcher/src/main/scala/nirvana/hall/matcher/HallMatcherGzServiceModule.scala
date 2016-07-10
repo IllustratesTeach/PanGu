e66740f4f1a79be35f2f4f6af04e40f3eb08efe5
songpeng <songpeng729@126.com>
2016-07-10 15:26:02 +0800

    hall-matcher GetMatchTaskServiceGzImpl

--- hall-matcher/src/main/scala/nirvana/hall/matcher/HallMatcherGzServiceModule.scala
+++ hall-matcher/src/main/scala/nirvana/hall/matcher/HallMatcherGzServiceModule.scala
@@ -2,9 +2,9 @@ package nirvana.hall.matcher
 
 import nirvana.hall.extractor.internal.FeatureExtractorImpl
 import nirvana.hall.extractor.services.FeatureExtractor
-import nirvana.hall.matcher.internal.adapter.common.{GetMatchTaskServiceImpl, PutMatchProgressServiceImpl}
-import nirvana.hall.matcher.internal.adapter.gz.{SyncDataServiceImpl, PutMatchResultServiceImpl}
-import nirvana.hall.matcher.service.{PutMatchProgressService, PutMatchResultService, GetMatchTaskService, SyncDataService}
+import nirvana.hall.matcher.internal.adapter.common.PutMatchProgressServiceImpl
+import nirvana.hall.matcher.internal.adapter.gz.{GetMatchTaskServiceGzImpl, PutMatchResultServiceImpl, SyncDataServiceImpl}
+import nirvana.hall.matcher.service.{GetMatchTaskService, PutMatchProgressService, PutMatchResultService, SyncDataService}
 import org.apache.tapestry5.ioc.ServiceBinder
 
 /**
@@ -14,7 +14,7 @@ object HallMatcherGzServiceModule {
 
   def bind(binder: ServiceBinder): Unit = {
     binder.bind(classOf[SyncDataService], classOf[SyncDataServiceImpl])
-    binder.bind(classOf[GetMatchTaskService], classOf[GetMatchTaskServiceImpl])
+    binder.bind(classOf[GetMatchTaskService], classOf[GetMatchTaskServiceGzImpl])
     binder.bind(classOf[PutMatchResultService], classOf[PutMatchResultServiceImpl])
     binder.bind(classOf[PutMatchProgressService], classOf[PutMatchProgressServiceImpl])
     //特征转换service
