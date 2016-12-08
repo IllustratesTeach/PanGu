b8dfaafa93a011fc1bbaa8f3cbdf3905c6cc12c0
songpeng <songpeng729@126.com>
2016-12-08 16:46:05 +0800

    hall-api 比对关系和获取查询信息单元测试

--- hall-api/src/test/scala/nirvana/hall/v62/services/QueryServiceImplTest.scala
+++ hall-api/src/test/scala/nirvana/hall/v62/services/QueryServiceImplTest.scala
@@ -3,6 +3,8 @@ package nirvana.hall.v62.services
 import nirvana.hall.api.services.QueryService
 import nirvana.hall.protocol.api.HallMatchRelationProto.MatchStatus
 import nirvana.hall.protocol.fpt.TypeDefinitionProto.MatchType
+import nirvana.hall.protocol.matcher.MatchTaskQueryProto.MatchTask
+import nirvana.hall.protocol.matcher.NirvanaTypeDefinition
 import nirvana.hall.v62.BaseV62TestCase
 import nirvana.hall.v62.internal.QueryServiceImpl
 import org.junit.{Assert, Test}
@@ -40,5 +42,26 @@ class QueryServiceImplTest extends BaseV62TestCase{
     val service = getService[QueryService]
     val matchResult = service.getMatchResult(1).get
     Assert.assertTrue(matchResult.getCandidateNum > 0)
+  }
+
+  @Test
+  def test_sendQuery: Unit ={
+    val matchTask = MatchTask.newBuilder()
+    matchTask.setMatchId("P3702000000002015129996")
+    matchTask.setMatchType(NirvanaTypeDefinition.MatchType.FINGER_TT)
+    matchTask.setPriority(1)
+    matchTask.setScoreThreshold(50)
+    matchTask.setObjectId(1)
+
+    val service = getService[QueryService]
+    val response = service.addMatchTask(matchTask.build())
+    Assert.assertNotNull(response)
+  }
+
+  @Test
+  def test_getQuery: Unit ={
+    val service = getService[QueryService]
+    val matchResult = service.getMatchResult(1)
+    Assert.assertNotNull(matchResult)
   }
 }
