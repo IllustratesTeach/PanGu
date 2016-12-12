ae1122d6ea7b8b0e373e5a537591ba02a8ecba91
songpeng <songpeng729@126.com>
2016-12-12 17:38:45 +0800

    hall-api 添加查询捺印人员编号列表方法getCardIdList

--- hall-api/src/main/scala/nirvana/hall/v62/internal/TPCardServiceImpl.scala
+++ hall-api/src/main/scala/nirvana/hall/v62/internal/TPCardServiceImpl.scala
@@ -1,10 +1,12 @@
 package nirvana.hall.v62.internal
 
 import nirvana.hall.api.services.TPCardService
+import nirvana.hall.c.services.gbaselib.gbasedef.GAKEYSTRUCT
 import nirvana.hall.c.services.gloclib.galoctp.GTPCARDINFOSTRUCT
 import nirvana.hall.protocol.api.FPTProto.TPCard
 import nirvana.hall.v62.config.HallV62Config
 import nirvana.hall.v62.internal.c.gloclib.galoctpConverter
+import nirvana.hall.v62.internal.c.gloclib.gcolnames._
 
 /**
  * Created by songpeng on 16/1/26.
@@ -68,9 +70,58 @@ class TPCardServiceImpl(facade:V62Facade,config:HallV62Config) extends TPCardSer
 
     galoctpConverter.convertGTPCARDINFOSTRUCT2ProtoBuf(tp)
   }
+
+  /**
+    * 查询捺印卡编号列表
+    *
+    * @param ryno        人员编号
+    * @param xm          姓名
+    * @param xb          性别
+    * @param idno        身份证号码
+    * @param zjlb        证件类别
+    * @param zjhm        证件号码
+    * @param hjddm       户籍地代码
+    * @param xzzdm       现住址代码
+    * @param rylb        人员类别
+    * @param ajlb        案件类别
+    * @param qkbs        前科标识
+    * @param xcjb        协查级别
+    * @param nydwdm      捺印单位代码
+    * @param startnydate 开始时间（检索捺印时间，时间格式YYYYMMDDHHMM）
+    * @param endnydate   结束时间（检索捺印时间，时间格式YYYYMMDDHHMM）
+    * @return
+    */
+  override def getCardIdList(ryno: String, xm: String, xb: String, idno: String, zjlb: String, zjhm: String, hjddm: String, xzzdm: String, rylb: String, ajlb: String, qkbs: String, xcjb: String, nydwdm: String, startnydate: String, endnydate: String): Seq[String] = {
+    val mapper = Map(
+      g_stCN.stTcID.pszName -> "szKey"
+    )
+    var statement = "(1=1)"
+    //TODO 补全所有查询条件，并支持like
+    if(isNonBlank(ryno)){
+      statement += " AND (cardid='%s')".format(ryno)
+    }
+    if(isNonBlank(xm)){
+      statement += " AND (name='%s')".format(xm)
+    }
+    if(isNonBlank(xb)){
+      statement += " AND (sexcode='%s')".format(xb)
+    }
+    if(isNonBlank(idno)){
+      statement += " AND (shenfenid='%s')".format(idno)
+    }
+
+    val cardIdList = facade.queryV62Table[GAKEYSTRUCT](V62Facade.DBID_TP_DEFAULT, V62Facade.TID_TPCARDINFO, mapper, Option(statement), 256)//最大返回256
+    cardIdList.map{ key =>
+      key.szKey
+    }
+  }
+
+  def isNonBlank(string: String):Boolean = string != null && string.length >0
+
   /**
    * 获取DBID
-   * @param dbId
+    *
+    * @param dbId
    */
   private def getDBID(dbId: Option[String]): Short ={
     if(dbId == None){
