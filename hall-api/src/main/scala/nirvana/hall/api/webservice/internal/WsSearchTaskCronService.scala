5a90f952aba0f0eca6b267eea78d115d55661105
yuchen_1997_200486@126.com <yuchen_1997_200486@126.com>
2016-12-12 09:47:36 +0800

    调用无锡十指查重任务的接口

--- hall-api/src/main/scala/nirvana/hall/api/webservice/internal/WsSearchTaskCronService.scala
+++ hall-api/src/main/scala/nirvana/hall/api/webservice/internal/WsSearchTaskCronService.scala
@@ -1,10 +1,19 @@
 package nirvana.hall.api.webservice.internal
 
+
+import javax.sql.DataSource
+import com.google.protobuf.ByteString
 import monad.support.services.LoggerSupport
 import nirvana.hall.api.config.HallApiConfig
+import nirvana.hall.api.services.sync.FetchQueryService
 import nirvana.hall.api.services.{CaseInfoService, LPCardService, QueryService, TPCardService}
 import nirvana.hall.api.webservice.services.WsSearchTaskService
+import nirvana.hall.api.webservice.util.FPTFileHandler
+import nirvana.hall.c.services.gfpt4lib.FPT4File.FPT4File
 import nirvana.hall.c.services.gfpt4lib.FPTFile
+import nirvana.hall.protocol.api.FPTProto.TPCard
+import nirvana.hall.protocol.extract.ExtractProto.ExtractRequest.FeatureType
+import nirvana.hall.protocol.extract.ExtractProto.FingerPosition
 import org.apache.tapestry5.ioc.annotations.PostInjection
 import org.apache.tapestry5.ioc.services.cron.{CronSchedule, PeriodicExecutor}
 import stark.webservice.services.StarkWebServiceClient
@@ -16,7 +25,9 @@ class WsSearchTaskCronService(hallApiConfig: HallApiConfig,
                               tPCardService: TPCardService,
                               lPCardService: LPCardService,
                               caseInfoService: CaseInfoService,
-                              queryService: QueryService) extends LoggerSupport{
+                              queryService: QueryService,
+                              fetchQueryService: FetchQueryService,
+                              implicit val dataSource: DataSource) extends LoggerSupport{
 
   val cronSchedule = "0 0/5 * * * ? *" //TODO 可配置
   /**
@@ -32,21 +43,176 @@ class WsSearchTaskCronService(hallApiConfig: HallApiConfig,
           val targetNamespace = "http://www.egfit.cn/"
           val userid = ""
           val password = ""
-
+          var taskControlID = ""
           val searchTaskService = StarkWebServiceClient.createClient(classOf[WsSearchTaskService], url, targetNamespace)
           val taskDataHandler = searchTaskService.getSearchTask(userid, password)
-          val taskFpt = FPTFile.parseFromInputStream(taskDataHandler.getInputStream)
 
-          //解析比对任务FPT
-          val taskControlID = taskFpt.right.get.sid
-          //图像解压
-          //提取特征
-          //数据入库
-          //发送查询任务
-          //发送更新比对任务状态请求, TODO 如果以上流程失败需要捕获异常，发送失败请求
-          searchTaskService.setSearchTaskStatus(userid, password, taskControlID, true)
+            try{
 
+              val taskFpt = FPTFile.parseFromInputStream(taskDataHandler.getInputStream)
+
+              //解析比对任务FPT
+              taskControlID = taskFpt.right.get.sid
+
+
+              taskFpt match {
+                case Left(fpt3) => throw new Exception("Not Support FPT-V3.0")
+                case Right(fpt4) =>
+                  if(fpt4.logic02Recs.length>0){
+                    if(fpt4.logic02Recs.foreach(tp => tp.head.dataType) == "02"){
+                      var tPCard:TPCard = null
+                      tPCard = TPFPT2ProtoBuffer(fpt4)
+                      tPCardService.addTPCard(tPCard)
+                      //TODO 发查询
+                      queryService.addMatchTask(null)
+                    }
+                  }else if(fpt4.logic03Recs.length>0){
+                    if(fpt4.logic03Recs.foreach(tp => tp.head.dataType) == "03"){
+                      // TODO 现场处理
+                    }
+                  }else{
+                      throw new Exception("接收FPT逻辑描述记录类型不正确")
+                  }
+              }
+              try{
+                val flag:Int = searchTaskService.setSearchTaskStatus(userid, password, taskControlID, true)
+//                if(flag!=1){
+//                  error("call setSearchTaskStatus faild!inputParam:"+ true + " returnVal:" + flag)
+//                }
+              }catch{
+                case e:Exception => error("setSearchTaskStatus-error:" + e.getMessage)
+              }
+          }catch{
+              case e:Exception=> error("WsSearchTaskCronService-error:" + e.getMessage)
+                try{
+                  val flag:Int = searchTaskService.setSearchTaskStatus(userid, password, taskControlID, false)
+//                  if(flag!=1){
+//                    case e:Exception=> error("call setSearchTaskStatus faild!inputParam:"+ false + " returnVal:" + flag)
+//                  }
+                }catch{
+                  case e:Exception=> error("setSearchTaskStatus-error:" + e.getMessage)
+                }
+          }
         }
       })
+  }
+
+
+
+
+
+  /**
+    * 根据接口获得的FPT文件,构建TPCard的ProtoBuffer的对象
+      * 在构建过程中,需要解压图片获得原图，提取特征
+    * @param fpt4
+    * @return
+    */
+  def TPFPT2ProtoBuffer(fpt4:FPT4File):TPCard ={
+    val tpCard = TPCard.newBuilder()
+    val textBuilder = tpCard.getTextBuilder
+        tpCard.setStrCardID(fpt4.sid)
+    fpt4.logic02Recs.foreach { tp =>
+      textBuilder.setStrName(tp.personName)
+      textBuilder.setStrAliasName (tp.alias)
+      textBuilder.setNSex(tp.gender.toInt)
+      textBuilder.setStrBirthDate (tp.birthday)
+      textBuilder.setStrIdentityNum (tp.idCardNo)
+      textBuilder.setStrBirthAddrCode (tp.addressDetail)
+      textBuilder.setStrBirthAddr (tp.address)
+      textBuilder.setStrAddrCode (tp.doorDetail)
+      textBuilder.setStrAddr (tp.door)
+      textBuilder.setStrPersonType (tp.category)
+      textBuilder.setStrCaseType1 (tp.caseClass1Code)
+      textBuilder.setStrCaseType2 (tp.caseClass2Code)
+      textBuilder.setStrCaseType3 (tp.caseClass3Code)
+      textBuilder.setStrPrintUnitCode (tp.gatherUnitCode)
+      textBuilder.setStrPrintUnitName (tp.gatherName)
+      textBuilder.setStrPrinter (tp.gatherName)
+      textBuilder.setStrPrintDate (tp.gatherDate)
+      textBuilder.setStrComment (tp.remark)
+      textBuilder.setStrNation (tp.nation)
+      textBuilder.setStrRace (tp.nativeplace)
+      textBuilder.setStrCertifType (tp.certificateType)
+      textBuilder.setStrCertifID (tp.certificateNo)
+      textBuilder.setBHasCriminalRecord (tp.isCriminal.toBoolean)
+      textBuilder.setStrCriminalRecordDesc (tp.criminalInfo)
+      textBuilder.setStrPremium (tp.assistUnitName)
+      textBuilder.setNXieChaFlag (tp.isAssist.toInt)
+      textBuilder.setStrXieChaRequestUnitName (tp.assistUnitName)
+      textBuilder.setStrXieChaRequestUnitCode (tp.assistUnitCode)
+      textBuilder.setNXieChaLevel (tp.assistLevel.toInt)
+      textBuilder.setStrXieChaForWhat (tp.assistPurpose)
+      textBuilder.setStrRelPersonNo (tp.relatedPersonId)
+      textBuilder.setStrRelCaseNo (tp.relatedCaseId)
+      textBuilder.setStrXieChaTimeLimit (tp.assistTimeLimit)
+      textBuilder.setStrXieChaDate (tp.assistDate)
+      textBuilder.setStrXieChaRequestComment (tp.assistAskingInfo)
+      textBuilder.setStrXieChaContacter (tp.contact)
+      textBuilder.setStrXieChaTelNo (tp.contactPhone)
+      textBuilder.setStrShenPiBy (tp.approver)
+
+
+      tp.fingers.foreach { tData =>
+        if (tData.imgData != null && tData.imgData.length > 0) {
+          val tBuffer = FPTFileHandler.fingerDataToGafisImage(tData)
+          //图像解压
+          val s = FPTFileHandler.callHallImageDecompressionImage(tBuffer)
+          //提取特征
+          val mnt = FPTFileHandler.extractorFeature(s,fgpParesEnum(tData.fgp),ParseFeatureTypeEnum(fpt4.tpCount.toInt,fpt4.lpCount.toInt))
+          val blobBuilder = tpCard.addBlobBuilder()
+          mnt.foreach { blob =>
+            blobBuilder.setStMntBytes(ByteString.copyFrom(blob.toByteArray()))
+          }
+        }
+      }
+      textBuilder.build()
+    }
+    tpCard.build()
+  }
+
+  /**
+    * 将解析出的指位翻译成系统中的枚举类型
+    */
+  def fgpParesEnum(fgp:String): FingerPosition ={
+    fgp match {
+      case "01" =>
+        FingerPosition.FINGER_R_THUMB
+      case "02" =>
+        FingerPosition.FINGER_R_INDEX
+      case "03" =>
+        FingerPosition.FINGER_R_MIDDLE
+      case "04" =>
+        FingerPosition.FINGER_R_RING
+      case "05" =>
+        FingerPosition.FINGER_R_LITTLE
+      case "06" =>
+        FingerPosition.FINGER_L_THUMB
+      case "07" =>
+        FingerPosition.FINGER_L_INDEX
+      case "08" =>
+        FingerPosition.FINGER_L_MIDDLE
+      case "09" =>
+        FingerPosition.FINGER_L_RING
+      case "10" =>
+        FingerPosition.FINGER_L_LITTLE
+      case other =>
+        FingerPosition.FINGER_UNDET
+    }
+  }
+
+  /**
+    *
+    * @param tpCount
+    * @param lpCount
+    * @return
+    */
+  def ParseFeatureTypeEnum(tpCount:Integer,lpCount:Integer): FeatureType ={
+    var featureType:FeatureType = null
+    if(tpCount>0){
+      featureType = FeatureType.FingerTemplate
+    }else if(lpCount>0){
+      featureType = FeatureType.FingerLatent
+    }
+    featureType
   }
 }
