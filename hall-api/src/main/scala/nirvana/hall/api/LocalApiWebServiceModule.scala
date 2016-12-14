6aefb63a61b7729492dff019b1797627dd340fe7
songpeng <songpeng729@126.com>
2016-12-14 12:10:17 +0800

    hall-api 添加union4pfmip的webservice参数配置

--- hall-api/src/main/scala/nirvana/hall/api/LocalApiWebServiceModule.scala
+++ hall-api/src/main/scala/nirvana/hall/api/LocalApiWebServiceModule.scala
@@ -1,5 +1,6 @@
 package nirvana.hall.api
 
+import nirvana.hall.api.webservice.internal.Union4pfmipCronService
 import nirvana.hall.api.webservice.services.WsFingerService
 import nirvana.hall.api.webservice.services.internal.WsFingerServiceImpl
 import org.apache.tapestry5.ioc.ServiceBinder
@@ -10,5 +11,6 @@ import org.apache.tapestry5.ioc.ServiceBinder
 object LocalApiWebServiceModule {
   def bind(binder: ServiceBinder) {
     binder.bind(classOf[WsFingerService], classOf[WsFingerServiceImpl]).withSimpleId()
+    binder.bind(classOf[Union4pfmipCronService], classOf[Union4pfmipCronService]).eagerLoad()
   }
 }
