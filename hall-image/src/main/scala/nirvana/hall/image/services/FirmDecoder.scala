5b29c6df816a7e54f8fbc717bf521a09207c9590
songpeng <songpeng729@126.com>
2017-02-04 14:46:38 +0800

    FirmDecoder添加注释

--- hall-image/src/main/scala/nirvana/hall/image/services/FirmDecoder.scala
+++ hall-image/src/main/scala/nirvana/hall/image/services/FirmDecoder.scala
@@ -10,6 +10,9 @@ import nirvana.hall.c.services.gloclib.glocdef.GAFISIMAGESTRUCT
 trait FirmDecoder{
   /**
    * decode compressed data using firm's algorithm
+   * (大库处理fpt数据转换为GAFISIMAGESTRUCT时统一都加了一个头信息，解压时统一去掉头信息再解压
+   * 由于GFS1900压缩算法的数据本身已经包含头信息,如果是数据库存储的数据,若使用该方法解压的GFS压缩数据时就需要多加一个头信息
+   * 这种情况推荐使用decodeByGFS)
    * @return original image data
    */
   def decode(gafisImg:GAFISIMAGESTRUCT): GAFISIMAGESTRUCT
