02c60ca31b72fa1443368baa2974fd807a69b424
kalel <a596773964@163.com>
2017-04-25 10:32:05 +0800

    调用接口

---
+++ hall-webservice/src/main/scala/nirvana/hall/webservice/util/WebservicesClient_XC.scala
@@ -0,0 +1,31 @@
+package nirvana.hall.webservice.util
+
+import javax.xml.namespace.QName
+
+import org.apache.axis2.addressing.EndpointReference
+import org.apache.axis2.client.Options
+import org.apache.axis2.rpc.client.RPCServiceClient
+
+/**
+  * Created by sjr on 2017/4/24.
+  */
+class WebservicesClient_AssistCheck {
+
+   def callHallWebService(opAddEntryArgs: Array[AnyRef], methodName:String): Int ={
+     val classes: Array[java.lang.Class[_]] = Array[java.lang.Class[_]](classOf[Int])
+    val serviceClientAndSetTaskStatus = createClient("http://127.0.0.1:8081" + "/ws/collectionSysPorts?wsdl"
+      ,"http://www.egfit.com/",methodName)
+    val flag = serviceClientAndSetTaskStatus.get._1.invokeBlocking(serviceClientAndSetTaskStatus.get._2, opAddEntryArgs, classes)(0).asInstanceOf[Int]
+    flag
+  }
+
+  private def createClient(url:String,targetNamespace:String,functionName:String): Option[(RPCServiceClient,QName)] ={
+    val serviceClient: RPCServiceClient = new RPCServiceClient
+    val options: Options = serviceClient.getOptions
+    val targetEPR: EndpointReference = new EndpointReference(url)
+    options.setTo(targetEPR)
+    val opAddEntry: QName = new QName(targetNamespace, functionName)
+    Some(serviceClient,opAddEntry)
+  }
+
+}
