ccd809333f7b7a7b6c4a91f72ef709320f00242c
songpeng <songpeng729@126.com>
2017-04-26 14:41:03 +0800

    添加hall-webservice启动脚本

---
+++ support/bin/hall-webservice.cmd
@@ -0,0 +1,20 @@
+@echo off
+REM Copyright 2016 the original author or authors. All rights reserved.
+REM site: http://www.ganshane.com
+
+setlocal
+call "%~dp0hall-env.cmd"
+
+set HALLMAIN=nirvana.hall.webservice.app.HallWebserviceApp
+
+REM set extra_module v62:nirvana.hall.v62.LocalV62Module,nirvana.hall.v62.LocalV62ServiceModule,stark.activerecord.StarkActiveRecordModule,nirvana.hall.v62.LocalV62DataSourceModule
+REM set extra_module v70:nirvana.hall.v70.LocalV70Module,nirvana.hall.v70.LocalV70ServiceModule,stark.activerecord.StarkActiveRecordModule,nirvana.hall.v70.LocalDataSourceModule
+set EXTRA_MODULE=nirvana.hall.v62.LocalV62Module,nirvana.hall.v62.LocalV62ServiceModule,stark.activerecord.StarkActiveRecordModule,nirvana.hall.v62.LocalV62DataSourceModule
+
+REM set java options
+SET JAVA_OPTIONS=%JAVA_OPTIONS% -Dserver.port=%SERVER_PORT%  -Dserver.home=%SERVER_HOME% -Dapi.extra.module=%EXTRA_MODULE% -Dfile.encoding=utf-8
+
+echo on
+call %JAVA% -cp "%CLASSPATH%" %JAVA_OPTIONS% %HALLMAIN%  %*
+
+endlocal
