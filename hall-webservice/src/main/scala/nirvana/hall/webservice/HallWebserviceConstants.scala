a6362dbd9dbc5753e6f1c879897fa02bd7799fef
liukai <aifirstlove163@msn.com>
2017-05-08 17:01:32 +0800

    增加FPT存储路径

--- hall-webservice/src/main/scala/nirvana/hall/webservice/HallWebserviceConstants.scala
+++ hall-webservice/src/main/scala/nirvana/hall/webservice/HallWebserviceConstants.scala
@@ -5,4 +5,10 @@ package nirvana.hall.webservice
   */
 object HallWebserviceConstants {
   val EXTRA_MODULE_CLASS = "extra.module"
+
+  //操作类型
+  val LocalTenFinger = "localTenprint" // 本地十指
+  val LocalLatent = "localLatent" //本地现场
+  val LocalHitResult = "localhitResult" //本地比对关系
+  val XCHitResult = "xchitresult" //外省查询比对关系
 }
