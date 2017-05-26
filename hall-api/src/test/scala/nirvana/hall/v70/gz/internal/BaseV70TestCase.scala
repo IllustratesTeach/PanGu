b0cb179207ea52c393817dcce3aef2d04b6e7b04
songpeng <songpeng729@126.com>
2017-05-26 20:07:00 +0800

    FPTExportApp 支持GZ

---
+++ hall-api/src/test/scala/nirvana/hall/v70/gz/internal/BaseV70TestCase.scala
@@ -0,0 +1,81 @@
+package nirvana.hall.v70.gz.internal
+
+import javax.persistence.EntityManagerFactory
+
+import monad.support.services.XmlLoader
+import nirvana.hall.api.config.HallApiConfig
+import nirvana.hall.api.internal.fpt.FPTServiceImpl
+import nirvana.hall.api.internal.remote.HallImageRemoteServiceImpl
+import nirvana.hall.api.internal.{AuthServiceImpl, FeatureExtractorImpl}
+import nirvana.hall.api.services._
+import nirvana.hall.api.services.fpt.FPTService
+import nirvana.hall.api.services.remote.HallImageRemoteService
+import nirvana.hall.extractor.services.FeatureExtractor
+import nirvana.hall.image.internal.{FirmDecoderImpl, ImageEncoderImpl}
+import nirvana.hall.image.services.{FirmDecoder, ImageEncoder}
+import nirvana.hall.v70.config.HallV70Config
+import org.apache.tapestry5.ioc.{Configuration, Registry, RegistryBuilder, ServiceBinder}
+import org.junit.{After, Before}
+import org.springframework.orm.jpa.{EntityManagerFactoryUtils, EntityManagerHolder}
+import org.springframework.transaction.support.TransactionSynchronizationManager
+
+import scala.io.Source
+import scala.reflect.{ClassTag, _}
+
+/**
+  * Created by songpeng on 2017/5/26.
+  */
+class BaseV70TestCase {
+
+  private var registry:Registry = _
+  protected def getService[T:ClassTag]:T={
+    registry.getService(classTag[T].runtimeClass.asInstanceOf[Class[T]])
+  }
+  @Before
+  def setup: Unit ={
+    val modules = Seq[String](
+      "stark.activerecord.StarkActiveRecordModule",
+      "nirvana.hall.v70.gz.LocalV70ServiceModule",
+      "nirvana.hall.v70.LocalDataSourceModule",
+      "nirvana.hall.api.LocalProtobufModule",
+      "monad.rpc.LocalRpcModule",
+      "nirvana.hall.v70.gz.internal.TestV70Module"
+    ).map(Class.forName)
+    registry = RegistryBuilder.buildAndStartupRegistry(modules: _*)
+    //OpenSession In Thread
+    val entityManagerFactory= getService[EntityManagerFactory]
+    val emHolder= new EntityManagerHolder(entityManagerFactory.createEntityManager())
+    TransactionSynchronizationManager.bindResource(entityManagerFactory, emHolder)
+  }
+  @After
+  def down: Unit ={
+    val emf: EntityManagerFactory = registry.getService(classOf[EntityManagerFactory])
+    val emHolder: EntityManagerHolder = TransactionSynchronizationManager.unbindResource(emf).asInstanceOf[EntityManagerHolder]
+    EntityManagerFactoryUtils.closeEntityManager(emHolder.getEntityManager)
+
+    registry.shutdown()
+  }
+}
+
+object TestV70Module{
+  def buildHallV70Config={
+    val content = Source.fromInputStream(getClass.getResourceAsStream("/test-v70.xml"),"utf8").mkString
+    XmlLoader.parseXML[HallV70Config](content, xsd = Some(getClass.getResourceAsStream("/nirvana/hall/v70/v70.xsd")))
+  }
+  def buildHallApiConfig={
+    new HallApiConfig
+  }
+  def bind(binder: ServiceBinder): Unit = {
+    binder.bind(classOf[AuthService], classOf[AuthServiceImpl])
+    //api 接口实现类
+    binder.bind(classOf[FeatureExtractor], classOf[FeatureExtractorImpl])
+    binder.bind(classOf[FirmDecoder],classOf[FirmDecoderImpl]).withId("FirmDecoder")
+    binder.bind(classOf[ImageEncoder],classOf[ImageEncoderImpl]).withId("ImageEncoder")
+    binder.bind(classOf[HallImageRemoteService], classOf[HallImageRemoteServiceImpl])
+    binder.bind(classOf[FPTService], classOf[FPTServiceImpl])
+  }
+  def contributeEntityManagerFactory(configuration:Configuration[String]): Unit ={
+    configuration.add("nirvana.hall.v70.gz.jpa")
+//    configuration.add("nirvana.hall.api.jpa")
+  }
+}
