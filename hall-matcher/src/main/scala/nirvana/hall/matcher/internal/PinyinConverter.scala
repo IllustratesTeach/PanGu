7f3762f4266750c15b89f834af8ea84ab9d9f349
songpeng <songpeng729@126.com>
2017-06-06 13:42:30 +0800

    add PinyinConverter

---
+++ hall-matcher/src/main/scala/nirvana/hall/matcher/internal/PinyinConverter.scala
@@ -0,0 +1,25 @@
+package nirvana.hall.matcher.internal
+
+import net.sourceforge.pinyin4j.PinyinHelper
+import net.sourceforge.pinyin4j.format.{HanyuPinyinCaseType, HanyuPinyinOutputFormat, HanyuPinyinToneType}
+
+/**
+  * Created by songpeng on 2017/5/31.
+  */
+object PinyinConverter {
+
+  /**
+    * 将汉字转换为拼音，空格分割，对不能转换的字符忽略
+    * @param str
+    * @return
+    */
+  def convert2Pinyin(str: String): String ={
+    if(str == null || str.length == 0)
+      return str
+    val format = new HanyuPinyinOutputFormat
+    format.setCaseType(HanyuPinyinCaseType.LOWERCASE)
+    format.setToneType(HanyuPinyinToneType.WITHOUT_TONE)
+    //这里str追加一个. 来保证最后的拼音是分开的
+    PinyinHelper.toHanYuPinyinString(str+".", format, " ", true)
+  }
+}
