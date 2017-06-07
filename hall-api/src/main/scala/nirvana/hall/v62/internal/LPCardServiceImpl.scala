ad838698150b5e32c07edd36cd482a3c09f06b47
liukai <aifirstlove163@msn.com>
2017-06-07 17:29:46 +0800

    删除hallDataSource

--- hall-api/src/main/scala/nirvana/hall/v62/internal/LPCardServiceImpl.scala
+++ hall-api/src/main/scala/nirvana/hall/v62/internal/LPCardServiceImpl.scala
@@ -1,6 +1,5 @@
 package nirvana.hall.v62.internal
 
-import nirvana.hall.api.HallDatasource
 import nirvana.hall.api.services.{HallDatasourceService, LPCardService}
 import nirvana.hall.c.services.gloclib.galoclp.GLPCARDINFOSTRUCT
 import nirvana.hall.protocol.api.FPTProto.LPCard
@@ -22,8 +21,6 @@ class LPCardServiceImpl(facade:V62Facade,config:HallV62Config, hallDatasourceSer
     //转换为c的结构
     val gLPCard= galoclpConverter.convertProtoBuf2GLPCARDINFOSTRUCT(lpCard)
     //调用实现方法
-    val latFinger_HallDatasource=new HallDatasource(gLPCard.szCardID,"",ip_source,HallDatasource.SERVICE_TYPE_SYNC,HallDatasource.OPERATION_TYPE_ADD,HallDatasource.SERVICE_TYPE_SYNC,HallDatasource.OPERATION_TYPE_ADD)
-    hallDatasourceService.save(latFinger_HallDatasource,HallDatasource.TABLE_V62_LATFINGER)
     facade.NET_GAFIS_FLIB_Add(getDBID(dbId),
       V62Facade.TID_LATFINGER,
       lpCard.getStrCardID, gLPCard)
@@ -48,8 +45,6 @@ class LPCardServiceImpl(facade:V62Facade,config:HallV62Config, hallDatasourceSer
    */
   override def updateLPCard(lpCard: LPCard, dbId: Option[String]): Unit = {
     val gLPCard = galoclpConverter.convertProtoBuf2GLPCARDINFOSTRUCT(lpCard)
-    val latFinger_HallDatasource=new HallDatasource(gLPCard.szCardID,"",ip_source,HallDatasource.SERVICE_TYPE_SYNC,HallDatasource.OPERATION_TYPE_MODIFY,HallDatasource.SERVICE_TYPE_SYNC,HallDatasource.OPERATION_TYPE_MODIFY)
-    hallDatasourceService.save(latFinger_HallDatasource,HallDatasource.TABLE_V62_LATFINGER)
     facade.NET_GAFIS_FLIB_Update(getDBID(dbId),
       V62Facade.TID_LATFINGER,
       lpCard.getStrCardID, gLPCard)
@@ -61,8 +56,6 @@ class LPCardServiceImpl(facade:V62Facade,config:HallV62Config, hallDatasourceSer
    * @return
    */
   override def delLPCard(cardId: String, dbId: Option[String]): Unit = {
-    val latFinger_HallDatasource=new HallDatasource(cardId,"",ip_source,HallDatasource.SERVICE_TYPE_SYNC,HallDatasource.OPERATION_TYPE_DEL,HallDatasource.SERVICE_TYPE_SYNC,HallDatasource.OPERATION_TYPE_DEL)
-    hallDatasourceService.save(latFinger_HallDatasource,HallDatasource.TABLE_V62_LATFINGER)
     facade.NET_GAFIS_FLIB_Del(getDBID(dbId), V62Facade.TID_LATFINGER, cardId)
   }
 
