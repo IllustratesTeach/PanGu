5182dd35749cb36b883fe672d0f4a748eeda2502
shishijie <shishijie@live.com>
2017-11-07 13:13:59 +0800

    回滚测试方法

--- hall-api/src/test/scala/nirvana/hall/api/internal/PojoCreatorApp.scala
+++ hall-api/src/test/scala/nirvana/hall/api/internal/PojoCreatorApp.scala
@@ -17,7 +17,7 @@ object PojoCreatorApp {
       * 程序会在该工程target/dest/ 目录下生成对应的包路径
       * 建议使用h2数据库生成pojo,由于在连接Oracle数据库会查询所有用户的表信息,而且不同用户存在相同的表名，会有冲突
       */
-    val url = "jdbc:h2:~/test"
+    val url = "jdbc:h2:file:~/h2_db/gafis-gz/gafis"
     val user = "sa"
     val pwd = ""
     val pkg = "nirvana.hall.v70.gz.jpa"
