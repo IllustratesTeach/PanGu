3513a63963ee76c7a97c124c5e4cfe0fe32cc0bb
songpeng <songpeng729@126.com>
2017-12-01 16:31:06 +0800

    删除脱机采集代码

--- hall-api/src/main/scala/nirvana/hall/v70/internal/blob/bytestream/BytePointer.java
+++
@@ -1,8 +0,0 @@
-package nirvana.hall.v70.internal.blob.bytestream;
-
-public class BytePointer
-{
-  public byte[] data;
-  public int offset;
-  public int length;
-}
--- hall-api/src/main/scala/nirvana/hall/v70/internal/blob/bytestream/IEgfStdCoder.java
+++
@@ -1,8 +0,0 @@
-package nirvana.hall.v70.internal.blob.bytestream;
-
-public abstract interface IEgfStdCoder
-{
-  public abstract int decode(IEgfStdStream paramIEgfStdStream, EgfStdField paramEgfStdField);
-
-  public abstract int encode(IEgfStdStream paramIEgfStdStream, EgfStdField paramEgfStdField);
-}
