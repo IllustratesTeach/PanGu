2a48e79f59e5ae61e8c9e3aeb3c9e735bf7e053e
yuchen <yuchen_1997_200486@126.com>
2018-02-02 10:23:23 +0800

    提交hallAPI v70 数据同步南京模块的相关代码。目前南京正在做数据库表和字段的整理工作。整理完成后，在此模块基础上进行二次开发和改造。

---
+++ hall-api/src/main/scala/nirvana/hall/v70/internal/adapter/nj/sys/UserServiceImpl.scala
@@ -0,0 +1,48 @@
+package nirvana.hall.v70.internal.adapter.nj.sys
+
+import nirvana.hall.api.services.AuthService
+import nirvana.hall.v70.common.jpa.SysUser
+import nirvana.hall.v70.services.sys.UserService
+import org.apache.commons.codec.digest.DigestUtils
+
+/**
+ * user service
+ * @author <a href="mailto:jcai@ganshane.com">Jun Tsai</a>
+ * @since 2015-06-02
+ */
+class UserServiceImpl(authService: AuthService) extends UserService {
+
+  override def testCreateUser(login: String, password: String): Unit = {
+    //SysUser.create("uuid","jcai",Some(DigestUtils.md5Hex(password)))
+    //SysUser.create("uuid","jcai",Some(DigestUtils.md5Hex(password)))
+    val user = new SysUser()
+    user.pkId = "UUID"
+    user.loginName = "jcai"
+    user.password = DigestUtils.md5Hex(password)
+
+    user.save()
+  }
+
+  override def existsLoginName(loginName: String): Boolean = {
+    SysUser.where(SysUser.loginName === loginName).limit(1).nonEmpty
+  }
+  override def login(loginName: String, password: String): (Option[SysUser], Option[String]) = {
+
+    val passwordEncrypted = DigestUtils.md5Hex(password)
+    val userOpt = SysUser.find_by_loginName_and_password(loginName,passwordEncrypted).headOption
+
+    var tokenOpt: Option[String] = None
+    userOpt.foreach(x => tokenOpt = Some(authService.login(loginName)))
+
+    (userOpt, tokenOpt)
+  }
+
+  /**
+   * 根据登录名查找用户
+   * @param loginName
+   * @return
+   */
+  override def findSysUserByLoginName(loginName: String): Option[SysUser] = {
+    SysUser.where(SysUser.loginName === loginName).limit(1).headOption
+  }
+}
