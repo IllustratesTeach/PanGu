4d965303852c3316e250880ffd5c8f2f80e04e40
yuchen <yuchen_1997_200486@126.com>
2018-05-26 20:13:18 +0800

    修改WSQFileProvider类，增加读取文件服务器超时重试及将异常捕获写入消息队列中

--- hall-spark/src/main/scala/nirvana/hall/spark/internal/WSQFileProvider.scala
+++ hall-spark/src/main/scala/nirvana/hall/spark/internal/WSQFileProvider.scala
@@ -5,12 +5,14 @@ import java.io.File
 import nirvana.hall.c.services.gloclib.glocdef
 import nirvana.hall.c.services.gloclib.glocdef.GAFISIMAGESTRUCT
 import nirvana.hall.protocol.extract.ExtractProto.ExtractRequest.FeatureType
+import nirvana.hall.protocol.extract.ExtractProto.FingerPosition
 import nirvana.hall.spark.config.NirvanaSparkConfig
 import nirvana.hall.spark.services.FptPropertiesConverter.TemplateFingerConvert
-import nirvana.hall.spark.services.{ImageProvider, SparkFunctions, SysProperties, WSQPropertiesConverter}
-import nirvana.hall.spark.services.SparkFunctions.StreamEvent
+import nirvana.hall.spark.services._
+import nirvana.hall.spark.services.SparkFunctions.{StreamEvent, reportError}
 
 import scala.collection.mutable.ArrayBuffer
+import scala.util.control.NonFatal
 
 /**
   * Created by yuchen on 2018/4/19.
@@ -18,8 +20,6 @@ import scala.collection.mutable.ArrayBuffer
 class WSQFileProvider extends ImageProvider{
 
   private lazy val imageFileServer = SysProperties.getPropertyOption("fpt.file.server")
-
-  val REGEX = s"[R][0-9]+(_)(\\d{2})(.wsq)"
   /**
     * 请求图像数据
     *
@@ -28,27 +28,39 @@ class WSQFileProvider extends ImageProvider{
     * @return 消息序列
     */
   override def requestImage(parameter: NirvanaSparkConfig, message: String): Seq[(StreamEvent, GAFISIMAGESTRUCT)] = {
-    def fetchWSQFile():Seq[(StreamEvent, GAFISIMAGESTRUCT)] = {
+    def fetchWSQFile(seq:Int):Seq[(StreamEvent, GAFISIMAGESTRUCT)] = {
       var arrayBuffer = ArrayBuffer[(StreamEvent, GAFISIMAGESTRUCT)]()
-      if(!message.startsWith("http") && !"None".equals(imageFileServer.get)){
-        val wsqFilePath =  message
-        val wsqFileData = SparkFunctions.httpClient.download(imageFileServer.get + File.separator+ wsqFilePath)
-        val personId = wsqFilePath.split("_")(0).split("/")(1)
-        val fgp = getFingerPosition(wsqFilePath.split("_")(1).split("\\.")(0).toInt)
-        val hasPerson = GafisPartitionRecordsDakuSaver.queryPersonById(personId)
-        if (hasPerson.isEmpty) {
-          val person = WSQPropertiesConverter.wsqInfoToPersonConvert(wsqFilePath)
-          GafisPartitionRecordsDakuSaver.savePersonInfo(person)
+      try{
+        if(!message.startsWith("http") && !"None".equals(imageFileServer.get)){
+          val wsqFilePath =  message
+          val wsqFileData = SparkFunctions.httpClient.download(imageFileServer.get + File.separator+ wsqFilePath)
+          val personId = wsqFilePath.split("_")(0).split("/")(1)
+          val fgp = getFingerPosition(wsqFilePath.split("_")(1).split("\\.")(0).toInt)
+          val hasPerson = GafisPartitionRecordsDakuSaver.queryPersonById(personId)
+          if (hasPerson.isEmpty) {
+            val person = WSQPropertiesConverter.wsqInfoToPersonConvert(wsqFilePath)
+            GafisPartitionRecordsDakuSaver.savePersonInfo(person)
+          }
+          val event = new StreamEvent(wsqFilePath,personId,FeatureType.FingerTemplate, fgp, "", "", "")
+          val buffer= (event,wsqToGafisImage(wsqFileData))
+          arrayBuffer += buffer
         }
-        val event = new StreamEvent(wsqFilePath,personId,FeatureType.FingerTemplate, fgp, "", "", "")
-        val buffer= (event,wsqToGafisImage(wsqFileData))
-        arrayBuffer += buffer
-      }else{
-        throw new UnsupportedOperationException("fileName is error: " + message)
+      }catch{
+        case e:CallRpcException=>
+          if(seq == 4)  reportException(e)  else fetchWSQFile(seq+1)
+        case NonFatal(e)=>
+          reportException(e)
       }
       arrayBuffer
     }
-    fetchWSQFile
+
+    def reportException(e: Throwable) = {
+      e.printStackTrace(System.err)
+      val event = StreamEvent("", "",  FeatureType.FingerTemplate, FingerPosition.FINGER_UNDET,"","","")
+      reportError(parameter, RequestRemoteFileError(event, e.toString))
+      null
+    }
+    fetchWSQFile(1)
   }
 
 
@@ -60,6 +72,8 @@ class WSQFileProvider extends ImageProvider{
     gafisImg.stHead.nImgSize = gafisImg.bnData.length
     gafisImg
   }
+
+
 
   /**
     * 请求数据完整信息
