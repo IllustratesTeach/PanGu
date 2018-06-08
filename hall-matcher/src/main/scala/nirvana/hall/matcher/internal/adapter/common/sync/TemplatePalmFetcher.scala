f95fe53f23fc739c3813bb226e01089cbf220e98
songpeng <songpeng729@126.com>
2018-06-08 17:24:37 +0800

    hall-matcher 修正掌纹纹线数据遗漏

--- hall-matcher/src/main/scala/nirvana/hall/matcher/internal/adapter/common/sync/TemplatePalmFetcher.scala
+++ hall-matcher/src/main/scala/nirvana/hall/matcher/internal/adapter/common/sync/TemplatePalmFetcher.scala
@@ -17,7 +17,7 @@ class TemplatePalmFetcher(hallMatcherConfig: HallMatcherConfig, dataSource: Data
   //是否对纹线分库
   val hasRidge = hallMatcherConfig.mnt.hasRidge
   override val MAX_SEQ_SQL: String = "select max(t.seq) from gafis_gather_palm t "
-  override val MIN_SEQ_SQL: String = "select min(t.seq) from gafis_gather_palm t where t.group_id=0 and t.seq >"
+  override val MIN_SEQ_SQL: String = "select min(t.seq) from gafis_gather_palm t where t.seq >"
   override val SYNC_SQL: String = "select p.sid, t.fgp, t.group_id, t.gather_data, t.seq, p.deletag " +
     " from gafis_gather_palm t " +
     " left join gafis_person p on t.person_id=p.personid " +
