fb5a1dbe3307b40652180949db011de098f4f092
mengxin <605706647@qq.com>
2018-07-23 17:40:12 +0800

    山东一体化采集入库接口

--- hall-webservice/src/main/scala/nirvana/hall/webservice/HallWebserviceQualityModule.scala
+++ hall-webservice/src/main/scala/nirvana/hall/webservice/HallWebserviceQualityModule.scala
@@ -1,13 +1,5 @@
 package nirvana.hall.webservice
 
-import nirvana.hall.api.internal.FeatureExtractorImpl
-import nirvana.hall.api.internal.fpt.FPT5ServiceImpl
-import nirvana.hall.api.internal.remote.HallImageRemoteServiceImpl
-import nirvana.hall.api.services.fpt.FPT5Service
-import nirvana.hall.api.services.remote.HallImageRemoteService
-import nirvana.hall.extractor.services.FeatureExtractor
-import nirvana.hall.image.internal.{FirmDecoderImpl, ImageEncoderImpl}
-import nirvana.hall.image.services.{FirmDecoder, ImageEncoder}
 import nirvana.hall.webservice.internal.quality.{StrategyServiceImpl, WsQualityScoreServiceImpl}
 import nirvana.hall.webservice.internal.survey.gafis62.SurveyTableMaintenanceService
 import nirvana.hall.webservice.services.quality.{StrategyService, WsQualityScoreService}
