63e45af293cec76df837d1b16a986e7389670d1c
liukai <aifirstlove163@msn.com>
2018-07-24 15:41:41 +0800

    修改现勘比中关系队列状态

--- hall-webservice/src/main/scala/nirvana/hall/webservice/internal/survey/gafis62/SurveyTableMaintenanceService.scala
+++ hall-webservice/src/main/scala/nirvana/hall/webservice/internal/survey/gafis62/SurveyTableMaintenanceService.scala
@@ -1,6 +1,7 @@
 package nirvana.hall.webservice.internal.survey.gafis62
 
 
+import nirvana.hall.api.internal.DataConverter
 import nirvana.hall.c.services.gloclib.survey.{SURVEYHITRESULTRECORD, SURVEYRECORD}
 import nirvana.hall.v62.config.HallV62Config
 import nirvana.hall.v62.internal.V62Facade
@@ -33,6 +34,7 @@ class SurveyTableMaintenanceService(v62Facade: V62Facade
     var surveyRecord = new SURVEYRECORD
     surveyRecord = result.headOption.getOrElse(throw new Exception("not get struts"))
     surveyRecord.nState = value.toInt.toByte
+    surveyRecord.nSID = DataConverter.convertLongAsSixByteArray(sid.toLong)
 
     v62Facade.NET_GAFIS_SURVEYRECORD_UPDATE(V62Facade.DBID_SURVEY
       , V62Facade.TID_SURVEYRECORD
@@ -42,7 +44,7 @@ class SurveyTableMaintenanceService(v62Facade: V62Facade
 
   def updateSurveyHitResultStateByOraSid(sid:String,value:String): Unit ={
     val statement = Option("($$SID=%s)".format(sid))
-    val mapper = Map("FINGERID"->"szFingerid"
+    val mapper = Map("FINGERID"->"szFingerID"
       ,"HITFINGERID" -> "szHitFingerID"
       ,"ORASID" -> "nOraSID"
       ,"QUEUETYPE" -> "nQueryType"
@@ -59,7 +61,7 @@ class SurveyTableMaintenanceService(v62Facade: V62Facade
     var surveyHitResultRecord = new SURVEYHITRESULTRECORD
     surveyHitResultRecord = result.headOption.getOrElse(throw new Exception("not get struts"))
     surveyHitResultRecord.nState = value.toInt.toByte
-
+    surveyHitResultRecord.nSID = DataConverter.convertLongAsSixByteArray(sid.toLong)
     v62Facade.NET_GAFIS_SURVEYHITRESULTRECORD_UPDATE(V62Facade.DBID_SURVEY
       , V62Facade.TID_SURVEYHITRESULTRECORD
       ,surveyHitResultRecord)
