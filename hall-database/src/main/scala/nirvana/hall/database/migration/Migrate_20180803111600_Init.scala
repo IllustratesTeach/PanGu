339efd8e8eaf7a81a4af7f9b43dc25c06daea4f4
liukai <aifirstlove163@msn.com>
2018-09-11 15:35:34 +0800

    江西辽宁现勘对接程序调整

--- hall-database/src/main/scala/nirvana/hall/database/migration/Migrate_20180803111600_Init.scala
+++ hall-database/src/main/scala/nirvana/hall/database/migration/Migrate_20180803111600_Init.scala
@@ -61,7 +61,7 @@ class Migrate_20180803111600_Init extends Migration {
       t.column("XCWZBH", VarcharType, Limit(30), Nullable, Comment("现场物证编号必填"))
       t.column("RESULTTYPE", VarcharType, Limit(1), Nullable, Comment("反馈信息----1：指掌纹系统建库 3：指纹比中 4：指纹未比中 8：指掌纹系统反馈无建库价值 9：指掌纹系统反馈数据包有问题"))
       t.column("CALLTIME", TimestampType, Nullable, Comment("反馈时间"))
-      t.column("RETURNSTATUS", VarcharType, Limit(1), Nullable, Comment("服务请求应答-----1：现场物证编号不存在 0：反馈失败 1：反馈成功"))
+      t.column("RETURNSTATUS", VarcharType, Limit(2), Nullable, Comment("服务请求应答-----1：现场物证编号不存在 0：反馈失败 1：反馈成功"))
       t.column("RETURNTIME", TimestampType, Nullable, Comment("应答时间"))
       t.column("ERRORMSG", ClobType, Nullable, Comment("异常信息记录----记录exception"))
     }
@@ -87,9 +87,23 @@ class Migrate_20180803111600_Init extends Migration {
     }
 
 
+//    createTable("LOG_PUTHITRESULT", Comment("发送比中信息主表----指纹系统调用现勘接口，上报比中关系fpt文件，无论是否成功，均在下表中记录信息")) { t =>
+//      t.column("PK_ID", VarcharType, Limit(32), NotNull, Comment("主键唯一主键，可用uuid"), PrimaryKey)
+//      t.column("USERNAME", VarcharType, Limit(20), Nullable, Comment("调用用户名我方用户名，默认AFIS_JZ"))
+//      t.column("HITRESULT_TYPE", VarcharType, Limit(2), Nullable, Comment("比中信息类型----上报比中信息类型，ll or lt"))
+//      t.column("XCZW_XCKYBH", VarcharType, Limit(23), Nullable, Comment("现场指纹_现场勘验编号"))
+//      t.column("XCZW_XCWZBH", VarcharType, Limit(30), Nullable, Comment("现场指纹_现场物证编号"))
+//      t.column("NYZW_YSXT_ASJXGRYBH", VarcharType, Limit(23), Nullable, Comment("捺印指纹_原始系统_案事件相关人员编号"))
+//      t.column("NYZW_ZZHWDM", VarcharType, Limit(2), Nullable, Comment("捺印指纹_指掌位代码"))
+//      t.column("ASJFSDD_XZQHDM", VarcharType, Limit(12), Nullable, Comment("案事件发生地点_行政区划代码----2101:沈阳  2102：大连"))
+//      t.column("CREATETIME", TimestampType, Nullable, Comment("创建日期"))
+//      t.column("UPDATETIME", TimestampType, Nullable, Comment("更新日期"))
+//      t.column("SENDHITRESULTCOUNT", IntegerType, Nullable, Comment("比中上报次数"))
+//    }
 
-    createTable("LOG_PUTHITRESULT", Comment("发送比中信息----指纹系统调用现勘接口，上报比中关系fpt文件，无论是否成功，均在下表中记录信息")) { t =>
+    createTable("LOG_PUTHITRESULT", Comment("发送比中信息表----指纹系统调用现勘接口，上报比中关系fpt文件，无论是否成功，均在下表中记录信息")) { t =>
       t.column("PK_ID", VarcharType, Limit(32), NotNull, Comment("主键唯一主键，可用uuid"), PrimaryKey)
+      //t.column("FID", VarcharType, Limit(32), NotNull, Comment("发送比中信息主表的PK_ID"))
       t.column("USERNAME", VarcharType, Limit(20), Nullable, Comment("调用用户名我方用户名，默认AFIS_JZ"))
       t.column("HITRESULT_TYPE", VarcharType, Limit(2), Nullable, Comment("比中信息类型----上报比中信息类型，ll or lt"))
       t.column("XCZW_ASJBH", VarcharType, Limit(30), Nullable, Comment("现场指纹_案事件编号"))
@@ -153,6 +167,29 @@ class Migrate_20180803111600_Init extends Migration {
       t.column("RETURNSTATUS", VarcharType, Limit(1), Nullable, Comment("接口方返回标记"))
       t.column("ERRORMSG", ClobType, Nullable, Comment("异常信息记录----记录exception"))
     }
+
+    createTable("LOG_INTERFACESTATUS", Comment("金指服务状态----指纹系统各个接口服务状态")) { t =>
+      t.column("PK_ID", VarcharType, Limit(32), NotNull, Comment("主键唯一主键，可用uuid"), PrimaryKey)
+      t.column("INTERFACENAME", VarcharType, Limit(30), Nullable, Comment("调用接口程序名称"))
+      t.column("SCHEDULE", VarcharType, Limit(40), Nullable, Comment("调用程序定时周期"))
+      t.column("ASJFSDD_XZQHDM", IntegerType, Limit(12), Nullable, Comment("2101:沈阳  2102：大连 ……"))
+      t.column("CALLTIME", TimestampType, Nullable, Comment("请求时间"))
+    }
+
+    createTable("LOG_HAIXINSERVICESTATUS", Comment("海鑫服务状态----海鑫各地市服务状态")) { t =>
+      t.column("PK_ID", VarcharType, Limit(32), NotNull, Comment("主键唯一主键，可用uuid"), PrimaryKey)
+      t.column("INTERFACEADDR", VarcharType, Limit(200), Nullable, Comment("调用接口程序名称"))
+      t.column("STATUS", VarcharType, Limit(1), Nullable, Comment("调用程序定时周期"))
+      t.column("ASJFSDD_XZQHDM", IntegerType, Limit(12), Nullable, Comment("2101:沈阳  2102：大连 ……"))
+      t.column("CALLTIME", TimestampType, Nullable, Comment("最后一次调用服务时间"))
+    }
+
+    createTable("LOG_UPDATESTATUS", Comment("海鑫服务状态----海鑫各地市服务状态")) { t =>
+      t.column("PK_ID", VarcharType, Limit(32), NotNull, Comment("主键唯一主键，可用uuid"), PrimaryKey)
+      t.column("SVRADDR", VarcharType, Limit(20), Nullable, Comment("程序服务地址"))
+      t.column("ASJFSDD_XZQHDM", IntegerType, Limit(12), Nullable, Comment("2101:沈阳  2102：大连 ……"))
+    }
+
   }
 
   def down(): Unit = {}
