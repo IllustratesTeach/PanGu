d0924bd9b1bdf0e749bcd1277a9ec269b44c82ac
liukai <aifirstlove163@msn.com>
2018-09-18 18:03:19 +0800

    江西辽宁现勘对接程序调整

--- hall-api/src/main/scala/nirvana/hall/api/LocalApiServiceModule.scala
+++ hall-api/src/main/scala/nirvana/hall/api/LocalApiServiceModule.scala
@@ -1,7 +1,7 @@
 package nirvana.hall.api
 
 import nirvana.hall.api.internal._
-import nirvana.hall.api.internal.fpt.{FPT5ServiceImpl, FPTServiceImpl}
+import nirvana.hall.api.internal.fpt.FPTServiceImpl
 import nirvana.hall.api.internal.remote._
 import nirvana.hall.api.services._
 import nirvana.hall.api.services.fpt.{FPT5Service, FPTService}
@@ -9,6 +9,7 @@ import nirvana.hall.api.services.remote._
 import nirvana.hall.extractor.services.FeatureExtractor
 import nirvana.hall.image.internal.{FirmDecoderImpl, ImageEncoderImpl}
 import nirvana.hall.image.services.{FirmDecoder, ImageEncoder}
+import nirvana.hall.v62.internal.FPT5ServiceImpl
 import org.apache.tapestry5.ioc.ServiceBinder
 import org.apache.tapestry5.services.Core
 
