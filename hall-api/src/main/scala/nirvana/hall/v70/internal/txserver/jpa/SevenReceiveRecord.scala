5be5b600111cfb3ff6aaba6d56d7c993a6ae7554
yuchen <yuchen_1997_200486@126.com>
2019-03-04 10:32:51 +0800

    提交部分数据库表对应的JPA类

---
+++ hall-api/src/main/scala/nirvana/hall/v70/internal/txserver/jpa/SevenReceiveRecord.scala
@@ -0,0 +1,45 @@
+package nirvana.hall.v70.internal.txserver.jpa
+
+import javax.persistence.{Column, Entity, Id, Table}
+
+import stark.activerecord.services.{ActiveRecord, ActiveRecordInstance}
+
+/**
+  * @author <a href="yuchen_1997_200486@126.com">yuchen</a>.
+  * @since 2019/3/4
+  */
+object SevenReceiveRecord extends ActiveRecordInstance[SevenReceiveRecord]
+
+@Entity
+@Table(name="SEVEN_RECEIVE_RECORD")
+class SevenReceiveRecord extends ActiveRecord{
+  @Id
+  @Column(name = "id", nullable=false, length=32)
+  var id:java.lang.String = _
+  @Column(name = "station_code")
+  var stationCode :java.lang.String = _
+  @Column(name = "flag")
+  var flag:java.lang.Integer = _
+  @Column(name = "cardid")
+  var cardId:java.lang.String = _
+  @Column(name = "input_time")
+  var inputTime:java.util.Date =  _
+
+  def this(id:java.lang.String) {
+    this()
+    this.id = id
+  }
+
+  def this(id:java.lang.String
+           ,stationCode:java.lang.String
+           ,flag:java.lang.Integer
+           ,cardId:java.lang.String
+           ,inputTime:java.util.Date){
+    this()
+    this.id = id
+    this.stationCode = stationCode
+    this.flag = flag
+    this.cardId = cardId
+    this.inputTime = inputTime
+  }
+}
--- hall-api/src/main/scala/nirvana/hall/v70/internal/txserver/jpa/TxLog.scala
+++ hall-api/src/main/scala/nirvana/hall/v70/internal/txserver/jpa/TxLog.scala
@@ -22,11 +22,15 @@ class TxLog extends ActiveRecord {
   @Column(name = "levels", nullable = false)
   var levels: Int = _
   @Column(name = "staion_code", nullable = false, length = 12)
-  var staionCode: java.lang.String = _
+  var stationCode: java.lang.String = _
   @Column(name = "card_no", length = 22)
   var cardNo: java.lang.String = _
   @Column(name = "work_type")
   var workType: java.lang.Integer = _
+  @Column(name = "msg")
+  var msg:java.sql.Clob = _
+  @Column(name = "flag")
+  var flag:java.lang.Integer = _
   @Column(name = "init_time", length = 10)
   var initTime: java.util.Date = _
 
@@ -36,13 +40,15 @@ class TxLog extends ActiveRecord {
     this.id = id
   }
 
-  def this(id: java.lang.String, levels: Int, staionCode: java.lang.String, cardNo: java.lang.String, workType: java.lang.Integer, initTime: java.util.Date) {
+  def this(id: java.lang.String, levels: Int, stationCode: java.lang.String, cardNo: java.lang.String, workType: java.lang.Integer, msg:java.sql.Clob,flag:java.lang.Integer,initTime: java.util.Date) {
     this()
     this.id = id
     this.levels = levels
-    this.staionCode = staionCode
+    this.stationCode = stationCode
     this.cardNo = cardNo
     this.workType = workType
+    this.msg = msg
+    this.flag = flag
     this.initTime = initTime
   }
 }
