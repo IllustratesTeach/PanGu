224d46e257bf4ca2282c4cfa5c1fb02bc0b54b65
yuchen <yuchen_1997_200486@126.com>
2019-03-04 13:35:03 +0800

    提交7.2 捺印卡定时质检、发查询及定时同步候选及比对结果的的基础类

---
+++ hall-api/src/main/scala/nirvana/hall/v70/internal/txserver/sync/CronSyncResult.scala
@@ -0,0 +1,40 @@
+package nirvana.hall.v70.internal.txserver.sync
+
+import monad.core.services.{CronScheduleWithStartModel, StartAtDelay}
+import monad.support.services.LoggerSupport
+import nirvana.hall.api.config.HallApiConfig
+import nirvana.hall.api.services.sync.SyncCronService
+import org.apache.tapestry5.ioc.annotations.PostInjection
+import org.apache.tapestry5.ioc.services.cron.PeriodicExecutor
+
+/**
+  * @author <a href="yuchen_1997_200486@126.com">yuchen</a>.
+  * @since 2019/3/4
+  */
+class CronSyncResult(apiConfig: HallApiConfig) extends SyncCronService with LoggerSupport{
+
+
+  @PostInjection
+  def startUp(periodicExecutor: PeriodicExecutor, syncCronService: SyncCronService): Unit = {
+    if(apiConfig.sync.syncCron != null){
+      periodicExecutor.addJob(new CronScheduleWithStartModel(null, StartAtDelay), "sync-cron", new Runnable {
+        override def run(): Unit = {
+          info("begin sync-cron")
+          try{
+            syncCronService.doWork
+          }catch{
+            case e:Exception =>
+              error("CronSyncResult error:{}",e.getMessage,e)
+          }
+          info("end sync-cron")
+        }
+      })
+    }
+  }
+
+
+  /**
+    * 定时任务调用方法
+    */
+  override def doWork(): Unit = ???
+}
