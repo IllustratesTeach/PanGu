e3626a8124accb203659309cfcc61f98c80ce3dc
liukai <aifirstlove163@msn.com>
2019-06-21 16:33:24 +0800

    上海妙手

--- hall-api/src/main/scala/nirvana/hall/v62/internal/c/gnetlib/netmisc.scala
+++ hall-api/src/main/scala/nirvana/hall/v62/internal/c/gnetlib/netmisc.scala
@@ -1,8 +1,12 @@
 package nirvana.hall.v62.internal.c.gnetlib
 
+import java.net.Socket
+
+import nirvana.hall.api.internal.DateConverter
+import nirvana.hall.c.services.gbaselib.gbasedef.AFISDateTime
 import nirvana.hall.c.services.ghpcbase.gnopcode._
 import nirvana.hall.c.services.gloclib.galocdbp.GAFIS_DEFDBIDSTRUCT
-import nirvana.hall.c.services.gloclib.glocndef.{GNETANSWERHEADOBJECT, GNETREQUESTHEADOBJECT}
+import nirvana.hall.c.services.gloclib.glocndef.{GAPPCONNECTIONSTRUCT, GNETANSWERHEADOBJECT, GNETREQUESTHEADOBJECT}
 import nirvana.hall.v62.internal.AncientClientSupport
 
 /**
@@ -22,6 +26,25 @@ trait netmisc {
     validateResponse(channel,pAns)
 
     new GAFIS_DEFDBIDSTRUCT().fromByteArray(pAns.bnData)
+  }
+
+  def NET_GAFIS_MISC_GetServerTime():AFISDateTime = executeInChannel{channel=>
+    val pReq = new GNETREQUESTHEADOBJECT
+    val pstCon = new GAPPCONNECTIONSTRUCT
+    val pAns = new GNETANSWERHEADOBJECT
+    //GAPPCONNECTIONSTRUCT stAppCon, *pstCon;
+    //GNETANSWERHEADOBJECT stAns, *pAns;
+    //GNETREQUESTHEADOBJECT stReq, *pReq;
+    NETREQ_SetOpClass(pReq, OP_CLASS_MISC);
+    NETREQ_SetOpCode(pReq, OP_MISC_GETSERVERTIME);
+    NETOP_SENDREQ(channel, pReq);
+    NETOP_RECVANS(channel, pAns);
+    var retval = NETANS_GetRetVal(pAns)
+    if(retval > 0) {
+      return new AFISDateTime().fromByteArray(pAns.bnData)
+    }else{
+      return new AFISDateTime
+    }
   }
 
 }
